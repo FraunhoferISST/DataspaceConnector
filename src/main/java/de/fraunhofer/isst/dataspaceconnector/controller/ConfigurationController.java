844f71a34ebac92f283ac7ccb5bb6a55346017c6
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2020-10-14 10:01:42 +0200

    chore: expect a configuration string instead of IM object

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/ConfigurationController.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/ConfigurationController.java
@@ -3,6 +3,7 @@ package de.fraunhofer.isst.dataspaceconnector.controller;
 import de.fraunhofer.iais.eis.ConfigurationModel;
 import de.fraunhofer.isst.ids.framework.configuration.ConfigurationContainer;
 import de.fraunhofer.isst.ids.framework.configuration.ConfigurationUpdateException;
+import de.fraunhofer.isst.ids.framework.spring.starter.SerializerProvider;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 /**
  * This class provides endpoints for connector configurations via a connected config manager.
@@ -26,20 +29,23 @@ public class ConfigurationController {
     public static final Logger LOGGER = LoggerFactory.getLogger(ConfigurationController.class);
 
     private ConfigurationContainer configurationContainer;
+    private SerializerProvider serializerProvider;
 
     @Autowired
-    public ConfigurationController(ConfigurationContainer configurationContainer) {
+    public ConfigurationController(ConfigurationContainer configurationContainer, SerializerProvider serializerProvider) {
         this.configurationContainer = configurationContainer;
+        this.serializerProvider = serializerProvider;
     }
 
     @Hidden
     @RequestMapping(value = "/configuration", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<Object> updateConfiguration(@RequestBody ConfigurationModel configurationModel) {
+    public ResponseEntity<Object> updateConfiguration(@RequestBody String updatedConfiguration) {
         try {
+            ConfigurationModel configurationModel = serializerProvider.getSerializer().deserialize(updatedConfiguration, ConfigurationModel.class);
             configurationContainer.updateConfiguration(configurationModel);
             return new ResponseEntity<>("Configuration successfully updated.", HttpStatus.OK);
-        } catch (ConfigurationUpdateException e) {
+        } catch (ConfigurationUpdateException | IOException e) {
             LOGGER.error("Configuration could not be updated: {}", e.getMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
