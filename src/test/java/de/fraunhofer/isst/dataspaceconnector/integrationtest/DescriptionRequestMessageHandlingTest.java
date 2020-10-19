f264860be982699001716506dec63fd2043df419
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2020-10-19 10:30:48 +0200

    docs: Add Javadoc to test classes

--- src/test/java/de/fraunhofer/isst/dataspaceconnector/integrationtest/DescriptionRequestMessageHandlingTest.java
+++ src/test/java/de/fraunhofer/isst/dataspaceconnector/integrationtest/DescriptionRequestMessageHandlingTest.java
@@ -8,6 +8,7 @@ import de.fraunhofer.iais.eis.ids.jsonld.Serializer;
 import de.fraunhofer.isst.dataspaceconnector.model.ResourceMetadata;
 import de.fraunhofer.isst.dataspaceconnector.services.resource.OfferedResourceRepository;
 import de.fraunhofer.isst.dataspaceconnector.services.resource.OfferedResourceService;
+import de.fraunhofer.isst.ids.framework.spring.starter.TokenProvider;
 import de.fraunhofer.isst.ids.framework.util.MultipartStringParser;
 import org.junit.Assert;
 import org.junit.Test;
@@ -49,6 +50,9 @@ public class DescriptionRequestMessageHandlingTest {
 
     @Autowired
     private Serializer serializer;
+
+    @Autowired
+    private TokenProvider tokenProvider;
 
     @Test
     public void requestSelfDescription() throws Exception {
@@ -148,7 +152,7 @@ public class DescriptionRequestMessageHandlingTest {
                 "   \"ids:requestedElement\":null,\r\n" +
                 "   \"ids:securityToken\":{\r\n" +
                 "      \"@type\":\"ids:DynamicAttributeToken\",\r\n" +
-                "      \"ids:tokenValue\":\"eyJ0eXAiOiJKV1QiLCJraWQiOiJkZWZhdWx0IiwiYWxnIjoiUlMyNTYifQ.eyJpZHNfYXR0cmlidXRlcyI6eyJzZWN1cml0eV9wcm9maWxlIjp7ImF1ZGl0X2xvZ2dpbmciOjB9LCJtZW1iZXJzaGlwIjp0cnVlLCJpZHMtdXJpIjoiaHR0cDovL3NvbWUtdXJpIiwidHJhbnNwb3J0X2NlcnRzX3NoYTI1OCI6ImJhY2I4Nzk1NzU3MzBiYjA4M2YyODNmZDViNjdhOGNiODk2OTQ0ZDFiZTI4YzdiMzIxMTdjZmM3NTdjODFlOTYifSwic2NvcGVzIjpbImlkc19jb25uZWN0b3IiXSwiYXVkIjoiSURTX0Nvbm5lY3RvciIsImlzcyI6Imh0dHBzOi8vZGFwcy5haXNlYy5mcmF1bmhvZmVyLmRlIiwic3ViIjoiQz1ERSxPPUZyYXVuaG9mZXIsT1U9SVNTVCxDTj01ODc3NmViZS1mOGY4LTRhNmYtYjQ0Yi1lZWVmYTQ3ZmMwNGIiLCJuYmYiOjE2MDIxNDQwNzMsImV4cCI6MTYwMjE0NzY3M30.SG1Av3G00ne2tYQMerrJbhg9f24klDMjS5ur1aykIGHrL5AyL2wsLit_5aMhG12DUQ7tPa2o4RHyTCQFAhVKkI9_bwCR9jGBcN6jfVn8vjxQ3mDvNdWOoRURI_3YOAjBlo1TqFLOKBmN3uTsB_ns7LqJDruea07sme5O38NOukHPWxsAnoiH4N9NByxHqxayrFj0buDxJCLKXG3_FQtZBcsGO89geylFec0epehh9pL5QV5nr4xLzVhfrJRgx512KVqr1hNLqfNRWGl0TFoKHyEE5J8IMEihZwF76_4kl_1HZe1HP866yO8ceONfTvRI2sCXmKpP8A02NGhisEF_Mg\",\r\n" +
+                "      \"ids:tokenValue\":\"" + tokenProvider.provideDapsToken() + "\",\r\n" +
                 "      \"referingConnector\":\"https://divaconnector.isst.fraunhofer.de\",\r\n" +
                 "      \"aud\":\"asd\",\r\n" +
                 "      \"iss\":\"adas\",\r\n" +
@@ -175,7 +179,7 @@ public class DescriptionRequestMessageHandlingTest {
                 "   \"ids:requestedElement\":\"https://w3id.org/idsa/autogen/dataResource/" + requestedArtifact.toString() + "\",\r\n" +
                 "   \"ids:securityToken\":{\r\n" +
                 "      \"@type\":\"ids:DynamicAttributeToken\",\r\n" +
-                "      \"ids:tokenValue\":\"eyJ0eXAiOiJKV1QiLCJraWQiOiJkZWZhdWx0IiwiYWxnIjoiUlMyNTYifQ.eyJpZHNfYXR0cmlidXRlcyI6eyJzZWN1cml0eV9wcm9maWxlIjp7ImF1ZGl0X2xvZ2dpbmciOjB9LCJtZW1iZXJzaGlwIjp0cnVlLCJpZHMtdXJpIjoiaHR0cDovL3NvbWUtdXJpIiwidHJhbnNwb3J0X2NlcnRzX3NoYTI1OCI6ImJhY2I4Nzk1NzU3MzBiYjA4M2YyODNmZDViNjdhOGNiODk2OTQ0ZDFiZTI4YzdiMzIxMTdjZmM3NTdjODFlOTYifSwic2NvcGVzIjpbImlkc19jb25uZWN0b3IiXSwiYXVkIjoiSURTX0Nvbm5lY3RvciIsImlzcyI6Imh0dHBzOi8vZGFwcy5haXNlYy5mcmF1bmhvZmVyLmRlIiwic3ViIjoiQz1ERSxPPUZyYXVuaG9mZXIsT1U9SVNTVCxDTj01ODc3NmViZS1mOGY4LTRhNmYtYjQ0Yi1lZWVmYTQ3ZmMwNGIiLCJuYmYiOjE2MDIxNDQwNzMsImV4cCI6MTYwMjE0NzY3M30.SG1Av3G00ne2tYQMerrJbhg9f24klDMjS5ur1aykIGHrL5AyL2wsLit_5aMhG12DUQ7tPa2o4RHyTCQFAhVKkI9_bwCR9jGBcN6jfVn8vjxQ3mDvNdWOoRURI_3YOAjBlo1TqFLOKBmN3uTsB_ns7LqJDruea07sme5O38NOukHPWxsAnoiH4N9NByxHqxayrFj0buDxJCLKXG3_FQtZBcsGO89geylFec0epehh9pL5QV5nr4xLzVhfrJRgx512KVqr1hNLqfNRWGl0TFoKHyEE5J8IMEihZwF76_4kl_1HZe1HP866yO8ceONfTvRI2sCXmKpP8A02NGhisEF_Mg\",\r\n" +
+                "      \"ids:tokenValue\":\"" + tokenProvider.provideDapsToken() + "\",\r\n" +
                 "      \"referingConnector\":\"https://divaconnector.isst.fraunhofer.de\",\r\n" +
                 "      \"aud\":\"asd\",\r\n" +
                 "      \"iss\":\"adas\",\r\n" +
@@ -191,5 +195,87 @@ public class DescriptionRequestMessageHandlingTest {
                 "   \"@id\":\"https://w3id.org/idsa/autogen/descriptionRequestMessage/3d621945-9839-4a2e-8437-4db7d1f959ca\"\r\n" +
                 "}\r\n";
     }
+
+//    private ResourceMetadata getResourceMetadata() {
+//        return new ResourceMetadata("Test resource", "", Arrays.asList("test", "resource"),
+//                "{\n" +
+//                        "  \"@context\" : {\n" +
+//                        "    \"ids\" : \"https://w3id.org/idsa/core/\"\n" +
+//                        "  },\n" +
+//                        "  \"@type\" : \"ids:ContractOffer\",\n" +
+//                        "  \"@id\" : \"https://w3id.org/idsa/autogen/contractOffer/110e659b-d171-4519-8a65-8a2c297ec296\",\n" +
+//                        "  \"ids:permission\" : [ {\n" +
+//                        "    \"@type\" : \"ids:Permission\",\n" +
+//                        "    \"@id\" : \"https://w3id.org/idsa/autogen/permission/7e1a166d-8c42-492f-afb8-204cea7aacf6\",\n" +
+//                        "    \"ids:description\" : [ {\n" +
+//                        "      \"@value\" : \"provide-access\",\n" +
+//                        "      \"@type\" : \"http://www.w3.org/2001/XMLSchema#string\"\n" +
+//                        "    } ],\n" +
+//                        "    \"ids:action\" : [ {\n" +
+//                        "      \"@id\" : \"idsc:USE\"\n" +
+//                        "    } ],\n" +
+//                        "    \"ids:title\" : [ {\n" +
+//                        "      \"@value\" : \"Example Usage Policy\",\n" +
+//                        "      \"@type\" : \"http://www.w3.org/2001/XMLSchema#string\"\n" +
+//                        "    } ]\n" +
+//                        "  } ]\n" +
+//                        "}",
+//                URI.create("http://resource-owner.com"), URI.create("http://license.com"), "v1.0",
+//                new ArrayList<>());
+//    }
+//
+//    private String getHeaderRequestedElementNull() {
+//        return "{\r\n" +
+//                "   \"@context\":\"https://w3id.org/idsa/contexts/2.0.0/context.jsonld\",\r\n" +
+//                "   \"@type\":\"ids:DescriptionRequestMessage\",\r\n" +
+//                "   \"ids:modelVersion\":\"4.0.0\",\r\n" +
+//                "   \"ids:issued\":\"2019-12-10T10:31:28.119+01:00\",\r\n" +
+//                "   \"ids:issuerConnector\":\"https://simpleconnector.ids.isst.fraunhofer.de/\",\r\n" +
+//                "   \"ids:requestedElement\":null,\r\n" +
+//                "   \"ids:securityToken\":{\r\n" +
+//                "      \"@type\":\"ids:DynamicAttributeToken\",\r\n" +
+//                "      \"ids:tokenValue\":\"eyJ0eXAiOiJKV1QiLCJraWQiOiJkZWZhdWx0IiwiYWxnIjoiUlMyNTYifQ.eyJpZHNfYXR0cmlidXRlcyI6eyJzZWN1cml0eV9wcm9maWxlIjp7ImF1ZGl0X2xvZ2dpbmciOjB9LCJtZW1iZXJzaGlwIjp0cnVlLCJpZHMtdXJpIjoiaHR0cDovL3NvbWUtdXJpIiwidHJhbnNwb3J0X2NlcnRzX3NoYTI1OCI6ImJhY2I4Nzk1NzU3MzBiYjA4M2YyODNmZDViNjdhOGNiODk2OTQ0ZDFiZTI4YzdiMzIxMTdjZmM3NTdjODFlOTYifSwic2NvcGVzIjpbImlkc19jb25uZWN0b3IiXSwiYXVkIjoiSURTX0Nvbm5lY3RvciIsImlzcyI6Imh0dHBzOi8vZGFwcy5haXNlYy5mcmF1bmhvZmVyLmRlIiwic3ViIjoiQz1ERSxPPUZyYXVuaG9mZXIsT1U9SVNTVCxDTj01ODc3NmViZS1mOGY4LTRhNmYtYjQ0Yi1lZWVmYTQ3ZmMwNGIiLCJuYmYiOjE2MDIxNDQwNzMsImV4cCI6MTYwMjE0NzY3M30.SG1Av3G00ne2tYQMerrJbhg9f24klDMjS5ur1aykIGHrL5AyL2wsLit_5aMhG12DUQ7tPa2o4RHyTCQFAhVKkI9_bwCR9jGBcN6jfVn8vjxQ3mDvNdWOoRURI_3YOAjBlo1TqFLOKBmN3uTsB_ns7LqJDruea07sme5O38NOukHPWxsAnoiH4N9NByxHqxayrFj0buDxJCLKXG3_FQtZBcsGO89geylFec0epehh9pL5QV5nr4xLzVhfrJRgx512KVqr1hNLqfNRWGl0TFoKHyEE5J8IMEihZwF76_4kl_1HZe1HP866yO8ceONfTvRI2sCXmKpP8A02NGhisEF_Mg\",\r\n" +
+//                "      \"referingConnector\":\"https://divaconnector.isst.fraunhofer.de\",\r\n" +
+//                "      \"aud\":\"asd\",\r\n" +
+//                "      \"iss\":\"adas\",\r\n" +
+//                "      \"sub\":\"sadas\",\r\n" +
+//                "      \"nbf\":\"asdasd\",\r\n" +
+//                "      \"exp\":\"1789722984\",\r\n" +
+//                "      \"ids:tokenFormat\":{\r\n" +
+//                "         \"@id\":\"https://w3id.org/idsa/code/JWT\"\r\n" +
+//                "      },\r\n" +
+//                "      \"@id\":\"https://w3id.org/idsa/autogen/dynamicAttributeToken/260ce6a6-3738-4cd2-9705-113301fdb111\"\r\n" +
+//                "   },\r\n" +
+//                "   \"ids:contentVersion\":\"4.0.0\",\r\n" +
+//                "   \"@id\":\"https://w3id.org/idsa/autogen/descriptionRequestMessage/3d621945-9839-4a2e-8437-4db7d1f959ca\"\r\n" +
+//                "}\r\n";
+//    }
+//
+//    private String getHeaderRequestedElementNotNull(UUID requestedArtifact) {
+//        return "{\r\n" +
+//                "   \"@context\":\"https://w3id.org/idsa/contexts/2.0.0/context.jsonld\",\r\n" +
+//                "   \"@type\":\"ids:DescriptionRequestMessage\",\r\n" +
+//                "   \"ids:modelVersion\":\"4.0.0\",\r\n" +
+//                "   \"ids:issued\":\"2019-12-10T10:31:28.119+01:00\",\r\n" +
+//                "   \"ids:issuerConnector\":\"https://simpleconnector.ids.isst.fraunhofer.de/\",\r\n" +
+//                "   \"ids:requestedElement\":\"https://w3id.org/idsa/autogen/dataResource/" + requestedArtifact.toString() + "\",\r\n" +
+//                "   \"ids:securityToken\":{\r\n" +
+//                "      \"@type\":\"ids:DynamicAttributeToken\",\r\n" +
+//                "      \"ids:tokenValue\":\"eyJ0eXAiOiJKV1QiLCJraWQiOiJkZWZhdWx0IiwiYWxnIjoiUlMyNTYifQ.eyJpZHNfYXR0cmlidXRlcyI6eyJzZWN1cml0eV9wcm9maWxlIjp7ImF1ZGl0X2xvZ2dpbmciOjB9LCJtZW1iZXJzaGlwIjp0cnVlLCJpZHMtdXJpIjoiaHR0cDovL3NvbWUtdXJpIiwidHJhbnNwb3J0X2NlcnRzX3NoYTI1OCI6ImJhY2I4Nzk1NzU3MzBiYjA4M2YyODNmZDViNjdhOGNiODk2OTQ0ZDFiZTI4YzdiMzIxMTdjZmM3NTdjODFlOTYifSwic2NvcGVzIjpbImlkc19jb25uZWN0b3IiXSwiYXVkIjoiSURTX0Nvbm5lY3RvciIsImlzcyI6Imh0dHBzOi8vZGFwcy5haXNlYy5mcmF1bmhvZmVyLmRlIiwic3ViIjoiQz1ERSxPPUZyYXVuaG9mZXIsT1U9SVNTVCxDTj01ODc3NmViZS1mOGY4LTRhNmYtYjQ0Yi1lZWVmYTQ3ZmMwNGIiLCJuYmYiOjE2MDIxNDQwNzMsImV4cCI6MTYwMjE0NzY3M30.SG1Av3G00ne2tYQMerrJbhg9f24klDMjS5ur1aykIGHrL5AyL2wsLit_5aMhG12DUQ7tPa2o4RHyTCQFAhVKkI9_bwCR9jGBcN6jfVn8vjxQ3mDvNdWOoRURI_3YOAjBlo1TqFLOKBmN3uTsB_ns7LqJDruea07sme5O38NOukHPWxsAnoiH4N9NByxHqxayrFj0buDxJCLKXG3_FQtZBcsGO89geylFec0epehh9pL5QV5nr4xLzVhfrJRgx512KVqr1hNLqfNRWGl0TFoKHyEE5J8IMEihZwF76_4kl_1HZe1HP866yO8ceONfTvRI2sCXmKpP8A02NGhisEF_Mg\",\r\n" +
+//                "      \"referingConnector\":\"https://divaconnector.isst.fraunhofer.de\",\r\n" +
+//                "      \"aud\":\"asd\",\r\n" +
+//                "      \"iss\":\"adas\",\r\n" +
+//                "      \"sub\":\"sadas\",\r\n" +
+//                "      \"nbf\":\"asdasd\",\r\n" +
+//                "      \"exp\":\"1789722984\",\r\n" +
+//                "      \"ids:tokenFormat\":{\r\n" +
+//                "         \"@id\":\"https://w3id.org/idsa/code/JWT\"\r\n" +
+//                "      },\r\n" +
+//                "      \"@id\":\"https://w3id.org/idsa/autogen/dynamicAttributeToken/260ce6a6-3738-4cd2-9705-113301fdb111\"\r\n" +
+//                "   },\r\n" +
+//                "   \"ids:contentVersion\":\"4.0.0\",\r\n" +
+//                "   \"@id\":\"https://w3id.org/idsa/autogen/descriptionRequestMessage/3d621945-9839-4a2e-8437-4db7d1f959ca\"\r\n" +
+//                "}\r\n";
+//    }
 
 }
