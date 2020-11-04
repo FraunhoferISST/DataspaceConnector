1b2bd451510a261fe210b4718e6be57f120239be
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2020-11-04 11:46:32 +0100

    fix: change column definition for large objects from "BLOB" to "BYTEA"

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/RequestedResource.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/RequestedResource.java
@@ -30,7 +30,7 @@ public class RequestedResource implements ConnectorResource{
     private Date modified;
 
     @NotNull
-    @Column(columnDefinition = "BLOB")
+    @Column(columnDefinition = "BYTEA")
     @JsonProperty("metadata")
     private ResourceMetadata resourceMetadata;
 
