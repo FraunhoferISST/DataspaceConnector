8a97dd68bc7851862b1ee412ae313891807f818e
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2020-12-07 10:12:48 +0100

    Updated signature and documentation of RequestedResourceService.

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resource/RequestedResourceService.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resource/RequestedResourceService.java
@@ -1,43 +1,43 @@
 package de.fraunhofer.isst.dataspaceconnector.services.resource;
 
 import de.fraunhofer.iais.eis.Resource;
-import de.fraunhofer.isst.dataspaceconnector.exceptions.InvalidResourceException;
 import de.fraunhofer.isst.dataspaceconnector.exceptions.ResourceException;
-import de.fraunhofer.isst.dataspaceconnector.exceptions.ResourceNotFoundException;
 import de.fraunhofer.isst.dataspaceconnector.model.RequestedResource;
 import de.fraunhofer.isst.dataspaceconnector.model.ResourceMetadata;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
 
 /**
  * <p>RequestedResourceService interface.</p>
  *
- * @author Julia Pampus
  * @version $Id: $Id
  */
 public interface RequestedResourceService {
+
     /**
      * <p>addResource.</p>
      *
-     * @param resourceMetadata a {@link de.fraunhofer.isst.dataspaceconnector.model.ResourceMetadata} object.
+     * @param resourceMetadata a {@link ResourceMetadata} object.
      * @return a {@link java.util.UUID} object.
+     * @throws ResourceException - if the resource could not be added.
      */
     UUID addResource(ResourceMetadata resourceMetadata) throws ResourceException;
 
     /**
      * <p>addData.</p>
      *
-     * @param id a {@link java.util.UUID} object.
+     * @param id   a {@link java.util.UUID} object.
      * @param data a {@link java.lang.String} object.
+     * @throws ResourceException - if the data could not be added.
      */
-    void addData(UUID id, String data) throws ResourceNotFoundException, InvalidResourceException;
+    void addData(UUID id, String data) throws ResourceException;
 
     /**
      * <p>deleteResource.</p>
      *
      * @param id a {@link java.util.UUID} object.
+     * @return true if the resource could be found and be deleted.
      */
     boolean deleteResource(UUID id);
 
@@ -45,28 +45,28 @@ public interface RequestedResourceService {
      * <p>getResource.</p>
      *
      * @param id a {@link java.util.UUID} object.
-     * @return a {@link de.fraunhofer.isst.dataspaceconnector.model.RequestedResource} object.
+     * @return a {@link RequestedResource} object.
+     * @throws ResourceException - if the resource could not be found.
      */
-    RequestedResource getResource(UUID id) throws InvalidResourceException;
+    RequestedResource getResource(UUID id) throws ResourceException;
 
     /**
      * <p>getMetadata.</p>
      *
      * @param id a {@link java.util.UUID} object.
-     * @return a {@link de.fraunhofer.isst.dataspaceconnector.model.ResourceMetadata} object.
+     * @return a {@link ResourceMetadata} object.
+     * @throws ResourceException - if the metadata could not be found.
      */
-    ResourceMetadata getMetadata(UUID id) throws ResourceNotFoundException,
-            InvalidResourceException;
+    ResourceMetadata getMetadata(UUID id) throws ResourceException;
 
     /**
      * <p>getData.</p>
      *
      * @param id a {@link java.util.UUID} object.
      * @return a {@link java.lang.String} object.
-     * @throws java.io.IOException if any.
+     * @throws ResourceException - if the data could not be received.
      */
-    String getData(UUID id) throws  InvalidResourceException, ResourceNotFoundException,
-            ResourceException;
+    String getData(UUID id) throws ResourceException;
 
     /**
      * <p>getRequestedResources.</p>
