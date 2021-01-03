1c08486d194b80575bafdc699febb8e6a69821b1
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-01-03 16:37:27 +0100

    chore: update configuration within broker communication + query as request body

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/BrokerController.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/BrokerController.java
@@ -1,18 +1,26 @@
 package de.fraunhofer.isst.dataspaceconnector.controller;
 
+import de.fraunhofer.iais.eis.BaseConnectorImpl;
+import de.fraunhofer.iais.eis.ConfigurationModelImpl;
+import de.fraunhofer.iais.eis.Resource;
+import de.fraunhofer.iais.eis.ResourceCatalogBuilder;
+import de.fraunhofer.iais.eis.util.Util;
 import de.fraunhofer.isst.dataspaceconnector.services.resources.OfferedResourceServiceImpl;
 import de.fraunhofer.isst.dataspaceconnector.services.resources.ResourceService;
 import de.fraunhofer.isst.ids.framework.configuration.ConfigurationContainer;
+import de.fraunhofer.isst.ids.framework.configuration.ConfigurationUpdateException;
 import de.fraunhofer.isst.ids.framework.spring.starter.BrokerService;
 import de.fraunhofer.isst.ids.framework.spring.starter.TokenProvider;
 import de.fraunhofer.isst.ids.framework.util.ClientProvider;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +28,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,6 +49,7 @@ public class BrokerController {
     private final TokenProvider tokenProvider;
     private final BrokerService brokerService;
     private final ResourceService resourceService;
+    private final ConfigurationContainer configurationContainer;
 
     /**
      * Constructor for BrokerController.
@@ -63,14 +73,14 @@ public class BrokerController {
 
         this.tokenProvider = tokenProvider;
         this.resourceService = offeredResourceService;
+        this.configurationContainer = configurationContainer;
 
         try {
             this.brokerService = new BrokerService(configurationContainer,
                 new ClientProvider(configurationContainer), tokenProvider);
         } catch (NoSuchAlgorithmException | KeyManagementException exception) {
-            LOGGER
-                .error("Failed to initialize the broker. Error in the framework. [exception=({})]",
-                    exception.getMessage());
+            LOGGER.error("Failed to initialize the broker. Error in the framework. "
+                    + "[exception=({})]", exception.getMessage());
             throw new GeneralSecurityException("Error in the framework.", exception);
         }
     }
@@ -91,6 +101,7 @@ public class BrokerController {
         // Make sure the request is authorized.
         if (tokenProvider.getTokenJWS() != null) {
             try {
+                updateConfigModel();
                 // Send the update request to the broker
                 final var brokerResponse = brokerService.updateAtBroker(url);
                 return new ResponseEntity<>("The broker answered with: "
@@ -98,6 +109,8 @@ public class BrokerController {
                     HttpStatus.OK);
             } catch (NullPointerException | IOException exception) {
                 return respondBrokerCommunicationFailed(exception);
+            } catch (ConfigurationUpdateException e) {
+                return respondUpdateError(url);
             }
         } else {
             // The request was unauthorized.
@@ -121,11 +134,14 @@ public class BrokerController {
         // Make sure the request is authorized.
         if (tokenProvider.getTokenJWS() != null) {
             try {
+                updateConfigModel();
                 // Send the unregister request to the broker
                 final var brokerResponse = brokerService.unregisterAtBroker(url);
                 return new ResponseEntity<>(brokerResponse.body().string(), HttpStatus.OK);
             } catch (NullPointerException | IOException exception) {
                 return respondBrokerCommunicationFailed(exception);
+            } catch (ConfigurationUpdateException e) {
+                return respondUpdateError(url);
             }
         } else {
             // The request was unauthorized.
@@ -145,16 +161,16 @@ public class BrokerController {
     public ResponseEntity<String> queryBroker(
         @Parameter(description = "The url of the broker.",
             required = true, example = "https://broker.ids.isst.fraunhofer.de/infrastructure")
-        @RequestParam("broker") String url) {
-        // Make sure the request is authorized.
-        if (tokenProvider.getTokenJWS() != null) {
-            // Send the query request to the broker
-            final var query = "SELECT ?subject ?predicate ?object\n" +
+        @RequestParam("broker") String url,
+        @Schema(description = "Database query (SparQL)", required = true,
+            example = "SELECT ?subject ?predicate ?object\n" +
                 "FROM <urn:x-arq:UnionGraph>\n" +
                 "WHERE {\n" +
                 "  ?subject ?predicate ?object\n" +
-                "};";
-
+                "};") @RequestBody String query) {
+        // Make sure the request is authorized.
+        if (tokenProvider.getTokenJWS() != null) {
+            // Send the query request to the broker
             try {
                 final var brokerResponse = brokerService.queryBroker(url, query,
                     null, null, null);
@@ -260,6 +276,18 @@ public class BrokerController {
         }
     }
 
+    private void updateConfigModel() throws ConfigurationUpdateException {
+        BaseConnectorImpl connector = (BaseConnectorImpl) configurationContainer.getConnector();
+        connector.setResourceCatalog(Util.asList(new ResourceCatalogBuilder()
+            ._offeredResource_((ArrayList<Resource>) resourceService.getResources())
+            .build()));
+
+        ConfigurationModelImpl configurationModel = (ConfigurationModelImpl) configurationContainer.getConfigModel();
+        configurationModel.setConnectorDescription(connector);
+
+        configurationContainer.updateConfiguration(configurationModel);
+    }
+
     /**
      * If the resource could not be found, reject and inform the requester.
      */
@@ -283,5 +311,13 @@ public class BrokerController {
     private ResponseEntity<String> respondRejectUnauthorized(String url) {
         LOGGER.debug("Unauthorized call. No DAT token found. [url=({})]", url);
         return new ResponseEntity<>("Please check your DAT token.", HttpStatus.UNAUTHORIZED);
+    }
+
+    /**
+     * If the configuration/connector could not be updated.
+     */
+    private ResponseEntity<String> respondUpdateError(String url) {
+        LOGGER.debug("Configuration error. Could not build current connector. [url=({})]", url);
+        return new ResponseEntity<>("Configuration error.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
