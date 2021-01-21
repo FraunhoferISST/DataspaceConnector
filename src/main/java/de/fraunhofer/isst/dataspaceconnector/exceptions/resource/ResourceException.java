07df647dfea60c5b188ed6dde7b11072c5877ada
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-01-21 12:22:57 +0100

    docs: add documentation to ResourceException

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/exceptions/resource/ResourceException.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/exceptions/resource/ResourceException.java
@@ -1,5 +1,8 @@
 package de.fraunhofer.isst.dataspaceconnector.exceptions.resource;
 
+/**
+ * Thrown to indicate that this problem regarding a resource occurred.
+ */
 public class ResourceException extends RuntimeException {
     //Default serial version uid
     private static final long serialVersionUID = 1L;
