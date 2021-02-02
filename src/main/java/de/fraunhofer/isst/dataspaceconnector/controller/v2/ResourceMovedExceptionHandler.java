d78ca7c882df01fe210507a87df45b1636a10fe2
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-02-02 09:50:40 +0100

    fix: change the response code for resource moved exception

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/v2/ResourceMovedExceptionHandler.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/v2/ResourceMovedExceptionHandler.java
@@ -1,6 +1,8 @@
 package de.fraunhofer.isst.dataspaceconnector.controller.v2;
 
 import de.fraunhofer.isst.dataspaceconnector.exceptions.resource.ResourceMovedException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,6 +12,9 @@ public final class ResourceMovedExceptionHandler {
     @ExceptionHandler(ResourceMovedException.class)
     public ResponseEntity<Void> handleResourceNotFoundException(
             final ResourceMovedException exception) {
-        return ResponseEntity.notFound().build();
+        var headers = new HttpHeaders();
+        headers.setLocation(exception.getNewEndpoint().toUri());
+
+        return new ResponseEntity<Void>(headers, HttpStatus.MOVED_PERMANENTLY);
     }
 }
--- src/main/java/de/fraunhofer/isst/dataspaceconnector/exceptions/resource/ResourceMovedException.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/exceptions/resource/ResourceMovedException.java
@@ -1,9 +1,17 @@
 package de.fraunhofer.isst.dataspaceconnector.exceptions.resource;
 
 import de.fraunhofer.isst.dataspaceconnector.model.v2.Endpoint;
+import de.fraunhofer.isst.dataspaceconnector.model.v2.EndpointId;
 
 public class ResourceMovedException extends RuntimeException {
+    EndpointId newEndpoint;
+
     public ResourceMovedException(final Endpoint newEndpoint) {
         super(newEndpoint.getId().toString());
+        this.newEndpoint = newEndpoint.getId();
+    }
+
+    public EndpointId getNewEndpoint() {
+        return newEndpoint;
     }
 }
