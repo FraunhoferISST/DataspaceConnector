187c8f9d4777a4ff1a3d2656149e070265b2c35e
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-02-03 07:55:44 +0100

    fix: modify the return message as of http 1.1 standard

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/v2/BaseResourceChildController.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/v2/BaseResourceChildController.java
@@ -3,6 +3,7 @@ package de.fraunhofer.isst.dataspaceconnector.controller.v2;
 import de.fraunhofer.isst.dataspaceconnector.model.v2.EndpointId;
 import de.fraunhofer.isst.dataspaceconnector.services.resources.v2.backendTofrontend.CommonUniDirectionalLinkerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,50 +20,103 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
+/**
+ * Offers REST-Api Endpoints for modifying relations between resources.
+ *
+ * @param <T> The service type for handling the relations logic.
+ */
 public class BaseResourceChildController
         <T extends CommonUniDirectionalLinkerService<?>> {
 
+    /**
+     * The linker between two resources.
+     **/
     @Autowired
     private T linker;
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    /**
+     * Default constructor.
+     */
+    protected BaseResourceChildController() {
+        // This constructor is intentionally empty. Nothing to do here.
+    }
+
+    /**
+     * Get all resources of the same type linked to the passed resource.
+     * Endpoint for GET requests.
+     *
+     * @param ownerId The id of the owning resource.
+     * @return The children of the resource.
+     */
+    @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<Set<EndpointId>> getResource(
-            @Valid @PathVariable final UUID id) {
-        return ResponseEntity.ok(linker.get(getCurrentEndpoint(id)));
+            @Valid @PathVariable final UUID ownerId) {
+        return ResponseEntity.ok(linker.get(getCurrentEndpoint(ownerId)));
     }
 
-    @PostMapping(value = "")
-    public ResponseEntity<Void> addResources(
-            @Valid @PathVariable final UUID id,
+    /**
+     * Add resources as children to a resource. Endpoint for POST requests.
+     *
+     * @param ownerId   The owning resource.
+     * @param resources The children to be added.
+     * @return Response with code 200 (Ok) and the new children's list.
+     */
+    @PostMapping
+    public ResponseEntity<Set<EndpointId>> addResources(
+            @Valid @PathVariable final UUID ownerId,
             @Valid @RequestBody final List<EndpointId> resources) {
-        linker.add(getCurrentEndpoint(id), new HashSet<>(resources));
-        return ResponseEntity.ok().build();
+        linker.add(getCurrentEndpoint(ownerId), new HashSet<>(resources));
+        // Send back the list of children after modification.
+        // See https://tools.ietf.org/html/rfc7231#section-4.3.3 and
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/200
+        return this.getResource(ownerId);
     }
 
-    @PutMapping(value = "")
+    /**
+     * Replace the children of a resource. Endpoint for PUT requests.
+     *
+     * @param ownerId   The id of the resource which children should be
+     *                  replaced.
+     * @param resources The resources that should be added as children.
+     * @return Response with code 204 (No_Content).
+     */
+    @PutMapping
     public ResponseEntity<Void> replaceResources(
-            @Valid @PathVariable final UUID id,
+            @Valid @PathVariable final UUID ownerId,
             @Valid @RequestBody final List<EndpointId> resources) {
-        linker.replace(getCurrentEndpoint(id), new HashSet<>(resources));
-        return ResponseEntity.ok().build();
+        linker.replace(getCurrentEndpoint(ownerId), new HashSet<>(resources));
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping(value = "")
+    /**
+     * Remove a list of children of a resource. Endpoint for DELETE requests.
+     *
+     * @param ownerId   The id of the resource which children should be removed.
+     * @param resources The list of children to be removed.
+     * @return Response with code 204 (No_Content).
+     */
+    @DeleteMapping
     public ResponseEntity<Void> removeResources(
-            @Valid @PathVariable final UUID id,
+            @Valid @PathVariable final UUID ownerId,
             @Valid @RequestBody final List<EndpointId> resources) {
-        linker.remove(getCurrentEndpoint(id), new HashSet<>(resources));
-        return ResponseEntity.ok().build();
+        linker.remove(getCurrentEndpoint(ownerId), new HashSet<>(resources));
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 
-    private EndpointId getCurrentEndpoint(final UUID id) {
+    /**
+     * Determines the current endpoint id from the request context.
+     *
+     * @param resourceId The resource id passed along the request.
+     * @return The Endpoint id.
+     */
+    private EndpointId getCurrentEndpoint(final UUID resourceId) {
         var basePath = ServletUriComponentsBuilder.fromCurrentRequest()
                 .build().toString();
 
-        final var index = basePath.lastIndexOf(id.toString());
+        final var index = basePath.lastIndexOf(resourceId.toString());
         // -1 so that the / gets also removed
         basePath = basePath.substring(0, index - 1);
 
-        return new EndpointId(basePath, id);
+        return new EndpointId(basePath, resourceId);
     }
 }
