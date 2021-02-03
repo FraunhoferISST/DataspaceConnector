ef0c1c2292dad2b461d3958cbbbffbca77accce1
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-02-03 16:10:53 +0100

    refactor: split update logic and document ContractFactory

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/v2/ContractFactory.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/v2/ContractFactory.java
@@ -1,14 +1,25 @@
 package de.fraunhofer.isst.dataspaceconnector.model.v2;
 
+import de.fraunhofer.isst.dataspaceconnector.services.utils.MetaDataUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 
+/**
+ * Creates and updates a contract.
+ */
 @Component
 public class ContractFactory implements BaseFactory<Contract, ContractDesc> {
+
+    /**
+     * Create a new contract.
+     *
+     * @param desc The description of the new contract.
+     * @return The new contract.
+     */
     @Override
     public Contract create(final ContractDesc desc) {
-        var contract = new Contract();
+        final var contract = new Contract();
         contract.setRules(new HashMap<>());
 
         update(contract, desc);
@@ -16,16 +27,23 @@ public class ContractFactory implements BaseFactory<Contract, ContractDesc> {
         return contract;
     }
 
+    /**
+     * Update a contract.
+     *
+     * @param contract The contract to be updated.
+     * @param desc     The new contract description.
+     * @return True if the contract has been modified.
+     */
     @Override
     public boolean update(final Contract contract, final ContractDesc desc) {
-        var hasBeenUpdated = false;
+        return this.updateTitle(contract, desc.getTitle());
+    }
 
-        var newTitle = desc.getTitle() != null ? desc.getTitle() : "";
-        if (!newTitle.equals(contract.getTitle())) {
-            contract.setTitle(newTitle);
-            hasBeenUpdated = true;
-        }
+    private boolean updateTitle(final Contract contract, final String title) {
+        final var newTitle = MetaDataUtils.updateString(contract.getTitle(),
+                title, "");
+        newTitle.ifPresent(contract::setTitle);
 
-        return hasBeenUpdated;
+        return newTitle.isPresent();
     }
 }
