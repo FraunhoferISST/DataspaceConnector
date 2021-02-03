0774e87623a08d1890c707cbf72b9cbdb58f7f1f
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2021-02-03 12:14:21 +0100

    fix: encode URL parameters, add null check for headers

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/services/utils/HttpUtils.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/utils/HttpUtils.java
@@ -18,6 +18,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 
