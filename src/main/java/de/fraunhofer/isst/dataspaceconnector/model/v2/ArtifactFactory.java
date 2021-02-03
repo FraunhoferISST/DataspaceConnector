f752b35b72dd758c39d8da385359dd178639831a
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-02-03 14:25:08 +0100

    refactor: move update logic into its own function

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/v2/ArtifactFactory.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/v2/ArtifactFactory.java
@@ -2,11 +2,23 @@ package de.fraunhofer.isst.dataspaceconnector.model.v2;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * This class creates and updates Artifact.
+ */
 @Component
-public class ArtifactFactory implements BaseFactory<Artifact, ArtifactDesc> {
+public final class ArtifactFactory implements BaseFactory<Artifact,
+        ArtifactDesc> {
+
+    /**
+     * Default constructor.
+     */
+    private ArtifactFactory() {
+        // This constructor is intentionally empty. Nothing to do here.
+    }
+
     @Override
     public Artifact create(final ArtifactDesc desc) {
-        var artifact = new Artifact();
+        final var artifact = new Artifact();
         update(artifact, desc);
 
         return artifact;
@@ -14,31 +26,42 @@ public class ArtifactFactory implements BaseFactory<Artifact, ArtifactDesc> {
 
     @Override
     public boolean update(final Artifact artifact, final ArtifactDesc desc) {
-        var hasBeenUpdated = false;
+        final var updatedTitle = updateTitle(artifact, desc.getTitle());
+        final var updatedData = updateData(artifact, desc);
 
-        var newTitle = desc.getTitle() != null ? desc.getTitle() : "";
-        if (!newTitle.equals(artifact.getTitle())) {
+        return updatedTitle || updatedData;
+    }
+
+    private boolean updateTitle(final Artifact artifact, final String title) {
+        final var newTitle = title != null ? title : "";
+        final var updateTitle = !newTitle.equals(artifact.getTitle());
+        if (updateTitle) {
             artifact.setTitle(newTitle);
-            hasBeenUpdated = true;
         }
 
-        if (desc.getAccessUrl() != null
-                && desc.getAccessUrl().toString().length() > 0) {
-            var data = new RemoteData();
+        return updateTitle;
+    }
+
+    private static boolean updateData(final Artifact artifact,
+                                      final ArtifactDesc desc) {
+        final var isRemoteData = desc.getAccessUrl() != null
+                && desc.getAccessUrl().toString().length() > 0;
+
+        // TODO : Check if the data is really updated.
+        if (isRemoteData) {
+            final var data = new RemoteData();
             data.setAccessUrl(desc.getAccessUrl());
             data.setUsername(desc.getUsername());
             data.setPassword(desc.getPassword());
 
             artifact.setData(data);
-            hasBeenUpdated = true;
+            return true;
         } else {
-            var data = new LocalData();
+            final var data = new LocalData();
             data.setValue(desc.getValue());
 
             artifact.setData(data);
-            hasBeenUpdated = true;
+            return true;
         }
-
-        return hasBeenUpdated;
     }
 }
