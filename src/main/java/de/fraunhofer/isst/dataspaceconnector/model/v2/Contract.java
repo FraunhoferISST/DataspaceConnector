0cd4a77a401580613764d7892bcd78db6a82d549
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-02-03 16:07:49 +0100

    docs: document Contract

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/v2/Contract.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/v2/Contract.java
@@ -12,14 +12,28 @@ import javax.persistence.Table;
 import java.util.Map;
 import java.util.UUID;
 
+/**
+ * A contract documents access and usage behaviours.
+ */
 @Data
 @Entity
 @Table
 @EqualsAndHashCode(callSuper = false)
 @Setter(AccessLevel.PACKAGE)
 public class Contract extends BaseResource {
+    /**
+     * Serial version uid.
+     **/
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The title of the contract.
+     */
     private String title;
 
+    /**
+     * The rules used by this contract.
+     **/
     @MapKey(name = "id")
     @OneToMany
     private Map<UUID, Rule> rules;
