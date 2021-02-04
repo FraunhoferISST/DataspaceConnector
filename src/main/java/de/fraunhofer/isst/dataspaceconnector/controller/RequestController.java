8a74906b9cf76b325a2ceb69320273be29c94808
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2021-02-04 08:02:55 +0100

    feat: add blank check for headers and params

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/RequestController.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/RequestController.java
@@ -323,7 +323,7 @@ public class RequestController {
         } catch (IllegalArgumentException exception) {
             // There is an empty key or value string in the params or headers map
             LOGGER.info("Invalid input for headers or params. [exception=({})]", exception.getMessage());
-            return new ResponseEntity<>("Invalid input for headers or params.",
+            return new ResponseEntity<>("Invalid input for headers or params: " + exception.getMessage(),
                     HttpStatus.BAD_REQUEST);
         }
 
@@ -428,27 +428,27 @@ public class RequestController {
     }
 
     /**
-     * Checks a given query input. If any of the keys or values in the headers or params maps are empty, an exception
-     * is thrown.
+     * Checks a given query input. If any of the keys or values in the headers or params maps are null, blank or empty,
+     * an  exception is thrown.
      *
      * @param queryInput the query input to validate.
-     * @throws IllegalArgumentException - if any of the keys or values are empty.
+     * @throws IllegalArgumentException - if any of the keys or values are null, blank or empty.
      */
     private void validateQueryInput(QueryInput queryInput) {
         if (queryInput != null && queryInput.getHeaders() != null) {
             for (Map.Entry<String, String> entry: queryInput.getHeaders().entrySet()) {
-                if (entry.getKey() == null || entry.getKey().isEmpty()
-                        || entry.getValue() == null || entry.getValue().isEmpty()) {
-                    throw new IllegalArgumentException("Header key or value should not be null or empty " +
+                if (entry.getKey() == null || entry.getKey().trim().isEmpty()
+                        || entry.getValue() == null || entry.getValue().trim().isEmpty()) {
+                    throw new IllegalArgumentException("Header key or value should not be null, blank or empty " +
                             "(key:" + entry.getKey() + ", value: " + entry.getValue() + ").");
                 }
             }
         }
         if (queryInput != null && queryInput.getParams() != null) {
             for (Map.Entry<String, String> entry: queryInput.getParams().entrySet()) {
-                if (entry.getKey() == null || entry.getKey().isEmpty()
-                        || entry.getValue() == null || entry.getValue().isEmpty()) {
-                    throw new IllegalArgumentException("Param key or value should not be null or empty.");
+                if (entry.getKey() == null || entry.getKey().trim().isEmpty()
+                        || entry.getValue() == null || entry.getValue().trim().isEmpty()) {
+                    throw new IllegalArgumentException("Param key or value should not be null, blank or empty.");
                 }
             }
         }
