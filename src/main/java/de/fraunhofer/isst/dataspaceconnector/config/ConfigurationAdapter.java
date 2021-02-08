be1bdd0bf9bb9ce6ffc3bb2931da601277981c2e
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-02-08 07:26:52 +0100

    chore: protect paths /api/** + add exception for /api/ids/data

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/config/ConfigurationAdapter.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/config/ConfigurationAdapter.java
@@ -3,6 +3,7 @@ package de.fraunhofer.isst.dataspaceconnector.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
@@ -18,12 +19,17 @@ public class ConfigurationAdapter extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .csrf().disable().formLogin().disable()
-            .antMatcher("/admin/**")
+            .antMatcher("/api/**")
             .authorizeRequests().anyRequest().hasRole("ADMIN")
             .and()
             .httpBasic()
             .authenticationEntryPoint(authenticationEntryPoint());
         http.headers().frameOptions().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().regexMatchers("/api/ids/data");
     }
 
     /**
