70e65951aff5755b8b4edd8147cee391e98a5103
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-02-10 08:20:25 +0100

    docs: add schema

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/QueryInput.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/QueryInput.java
@@ -1,9 +1,23 @@
 package de.fraunhofer.isst.dataspaceconnector.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.HashMap;
 
+@Schema(
+        name = "QueryInput",
+        description = "Query parameters as map",
+        oneOf = QueryInput.class,
+        example = "{\n" +
+                "  \"headers\": {\n" +
+                "    \"key\": \"value\"\n" +
+                "  },\n" +
+                "  \"params\": {\n" +
+                "    \"key\": \"value\"\n" +
+                "  }\n" +
+                "}"
+)
 @Data
 public class QueryInput {
 
