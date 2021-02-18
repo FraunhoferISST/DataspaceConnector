1ae0cb639ad1ab113099d64cfbddfd6b7e508fac
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-02-18 13:48:44 +0100

    docs: add docs to BaseFactory

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/BaseFactory.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/BaseFactory.java
@@ -1,6 +1,24 @@
 package de.fraunhofer.isst.dataspaceconnector.model;
 
+/**
+ * The base factory for factory classes.
+ * This class creates and updates an entity by using a supplied description.
+ * @param <T> The type of the entity.
+ * @param <D> The type of the description.
+ */
 public interface BaseFactory<T extends BaseEntity, D extends BaseDescription<T>> {
+    /**
+     * Create a new entity.
+     * @param desc The description of the entity.
+     * @return The new entity.
+     */
     T create(D desc);
-    boolean update(T type, D desc);
+
+    /**
+     * Update an entity.
+     * @param entity The entity to be updated.
+     * @param desc The description of the new entity.
+     * @return true if changes where performed.
+     */
+    boolean update(T entity, D desc);
 }
