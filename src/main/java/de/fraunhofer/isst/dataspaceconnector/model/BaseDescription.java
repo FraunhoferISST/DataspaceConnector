3c8536be4ba2588248b8f2a54c2080ef60cdf4ce
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-02-18 13:37:36 +0100

    docs: add docs to BaseDescription

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/BaseDescription.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/BaseDescription.java
@@ -4,7 +4,14 @@ import lombok.Data;
 
 import java.util.UUID;
 
+/**
+ * The base class for all descriptions.
+ * @param <T> The type of the class described by the description.
+ */
 @Data
 public class BaseDescription<T> {
+    /**
+     * The static id assigned to public endpoints.
+     */
     private UUID staticId;
 }
