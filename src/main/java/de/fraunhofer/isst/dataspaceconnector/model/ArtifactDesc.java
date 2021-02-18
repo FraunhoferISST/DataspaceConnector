b57c10d8b355556ef2531dc6cac479d2dc8f9719
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-02-18 13:23:29 +0100

    docs: add docs to ArtifactDesc

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/ArtifactDesc.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/ArtifactDesc.java
@@ -5,14 +5,36 @@ import lombok.EqualsAndHashCode;
 
 import java.net.URL;
 
+/**
+ * A description of an artifact.
+ * This class is consumed when creating or updating an artifact.
+ */
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class ArtifactDesc extends BaseDescription<Artifact> {
+
+    /**
+     * The title of the artifact.
+     */
     private String title;
 
+    /**
+     * The url of the data location.
+     */
     private URL accessUrl;
+
+    /**
+     * The username for authentication at the data location.
+     */
     private String username;
+
+    /**
+     * The password for authentication at the data location.
+     */
     private String password;
 
+    /**
+     * Some value for storing data locally.
+     */
     private String value;
 }
