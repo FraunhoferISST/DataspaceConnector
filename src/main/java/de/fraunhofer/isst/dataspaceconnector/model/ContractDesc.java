6237e4ff30e4c8da10b428453455dd83fb897c52
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-02-18 13:57:26 +0100

    docs: add docs to ContractDesc

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/ContractDesc.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/ContractDesc.java
@@ -3,8 +3,14 @@ package de.fraunhofer.isst.dataspaceconnector.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+/**
+ * Describes a contract's properties.
+ */
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class ContractDesc extends BaseDescription<Contract> {
+    /**
+     * The title of the contract.
+     */
     private String title;
 }
