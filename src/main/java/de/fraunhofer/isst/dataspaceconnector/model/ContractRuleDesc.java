88319e5f8799da321a0b180fdc1d4763a863bad4
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-02-18 14:06:10 +0100

    docs: add docs to ContractRuleDesc

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/ContractRuleDesc.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/ContractRuleDesc.java
@@ -3,9 +3,19 @@ package de.fraunhofer.isst.dataspaceconnector.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+/**
+ * The description of a contract rule.
+ */
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class ContractRuleDesc extends BaseDescription<ContractRule> {
+    /**
+     * The title of the rule.
+     */
     private String title;
+
+    /**
+     * The rule to be enforced.
+     */
     private String rule;
 }
