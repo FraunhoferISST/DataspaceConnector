ad76914c708865f2478b90aa5d51f8dfdfb801bc
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-02-19 09:48:44 +0100

    refactor: HttpTraceFilter

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/filter/httptracing/HttpTraceFilter.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/filter/httptracing/HttpTraceFilter.java
@@ -17,23 +17,30 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-
 /**
  * Use this class to log all incoming and outgoing http traffic.
  */
 @Component
 @Order(1)
 public class HttpTraceFilter extends OncePerRequestFilter {
-
+    /**
+     * The trace id.
+     */
     private UUID traceId;
+
+    /**
+     * The event handler.
+     */
     private final HttpTraceEventHandler eventHandler;
 
     /**
-     * Constructor
+     * The constructor.
      *
-     * @param eventHandler The handler responsible for HttpTrace events raised by this class
+     * @param eventHandler The handler responsible for HttpTrace events raised by this class.
      */
-    public HttpTraceFilter(HttpTraceEventHandler eventHandler) {
+    @SuppressWarnings("checkstyle:HiddenField")
+    public HttpTraceFilter(final HttpTraceEventHandler eventHandler) {
+        super();
         this.eventHandler = eventHandler;
     }
 
@@ -42,8 +49,10 @@ public class HttpTraceFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-        FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(final HttpServletRequest request,
+                                    final HttpServletResponse response,
+                                    final FilterChain filterChain)
+            throws ServletException, IOException {
         final var requestWrapper = new RequestWrapper(request);
         final var responseWrapper = new ContentCachingResponseWrapper(response);
 
@@ -58,7 +67,7 @@ public class HttpTraceFilter extends OncePerRequestFilter {
         }
     }
 
-    private void beforeRequest(RequestWrapper request) {
+    private void beforeRequest(final RequestWrapper request) {
         final var trace = new HttpTrace();
         trace.id = traceId;
         trace.timestamp = LocalDateTime.now();
@@ -67,10 +76,10 @@ public class HttpTraceFilter extends OncePerRequestFilter {
         trace.client = request.getRemoteAddr();
 
         trace.headers = "{";
-        var headerNames = request.getHeaderNames();
+        final var headerNames = request.getHeaderNames();
         while (headerNames.hasMoreElements()) {
-            var key = headerNames.nextElement();
-            var value = request.getHeader(key);
+            final var key = headerNames.nextElement();
+            final var value = request.getHeader(key);
             trace.headers += key + ": " + value;
 
             if (headerNames.hasMoreElements()) {
@@ -80,10 +89,10 @@ public class HttpTraceFilter extends OncePerRequestFilter {
         trace.headers += "}";
 
         trace.parameterMap = "{";
-        var parameterNames = request.getParameterNames();
+        final var parameterNames = request.getParameterNames();
         while (parameterNames.hasMoreElements()) {
-            var key = parameterNames.nextElement();
-            var value = request.getHeader(key);
+            final var key = parameterNames.nextElement();
+            final var value = request.getHeader(key);
             trace.parameterMap += key + ": " + value;
 
             if (parameterNames.hasMoreElements()) {
@@ -92,17 +101,16 @@ public class HttpTraceFilter extends OncePerRequestFilter {
         }
         trace.parameterMap += "}";
 
-        try{
+        try {
             trace.body = new String(request.getRequestBody(), StandardCharsets.UTF_8);
-        }catch(IOException exception){
+        } catch (IOException exception) {
             exception.printStackTrace();
         }
 
         eventHandler.sendHttpTraceEvent(trace);
     }
 
-    private void afterRequest(
-        ContentCachingResponseWrapper responseWrapper) {
+    private void afterRequest(final ContentCachingResponseWrapper responseWrapper) {
         final var trace = new HttpTrace();
         trace.id = traceId;
         trace.timestamp = LocalDateTime.now();
@@ -110,9 +118,9 @@ public class HttpTraceFilter extends OncePerRequestFilter {
         trace.body = getResponseAsPayload(responseWrapper);
 
         trace.headers = "{";
-        var headerNames = responseWrapper.getHeaderNames();
-        for (var key : headerNames) {
-            var value = responseWrapper.getHeader(key);
+        final var headerNames = responseWrapper.getHeaderNames();
+        for (final var key : headerNames) {
+            final var value = responseWrapper.getHeader(key);
             trace.headers += key + ": " + value;
 
             if (key != headerNames.toArray()[headerNames.toArray().length - 1]) {
@@ -124,17 +132,18 @@ public class HttpTraceFilter extends OncePerRequestFilter {
         eventHandler.sendHttpTraceEvent(trace);
     }
 
-    private String getResponseAsPayload(ContentCachingResponseWrapper wrappedResponse) {
+    private String getResponseAsPayload(final ContentCachingResponseWrapper wrappedResponse) {
+        String response = "";
         try {
-            if (wrappedResponse.getContentSize() <= 0) {
-                return "";
+            if (wrappedResponse.getContentSize() > 0) {
+                response = new String(wrappedResponse.getContentAsByteArray(), 0,
+                        wrappedResponse.getContentSize(),
+                        wrappedResponse.getCharacterEncoding());
             }
-
-            return new String(wrappedResponse.getContentAsByteArray(), 0,
-                wrappedResponse.getContentSize(),
-                wrappedResponse.getCharacterEncoding());
         } catch (UnsupportedEncodingException e) {
-            return "ERROR";
+            response = "ERROR";
         }
+
+        return response;
     }
 }
