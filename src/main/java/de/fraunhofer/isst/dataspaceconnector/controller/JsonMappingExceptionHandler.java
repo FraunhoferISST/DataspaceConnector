74050e63aec5ebf66d3e0bb784ed37f3c7122bec
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-03-02 19:23:13 +0100

    refactor: add javadocs to JsonMappingExceptionHandler

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/JsonMappingExceptionHandler.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/JsonMappingExceptionHandler.java
@@ -6,9 +6,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/**
+ * This class handles exceptions of type {@link JsonMappingException}.
+ */
 @RestControllerAdvice
 public class JsonMappingExceptionHandler {
-
+    /**
+     * Handles thrown {@link JsonMappingException}.
+     *
+     * @return A http response.
+     */
     @ExceptionHandler(JsonMappingException.class)
     public ResponseEntity<String> handleJsonMappingException() {
         return new ResponseEntity<>("Invalid input.", HttpStatus.BAD_REQUEST);
