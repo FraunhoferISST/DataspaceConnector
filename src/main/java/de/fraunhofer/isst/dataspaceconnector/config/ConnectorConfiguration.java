e2e89d4dc7877728e01ba7924530dc45794166ff
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-03-06 17:19:37 +0100

    refactor: connector configuration classes

---
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/config/ConnectorConfiguration.java
@@ -0,0 +1,39 @@
+package de.fraunhofer.isst.dataspaceconnector.config;
+
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This class handles policy settings: negotiation, pattern support, and usage control framework.
+ */
+@Data
+@Configuration
+@RequiredArgsConstructor
+public class ConnectorConfiguration {
+    /**
+     * The clearing house access url.
+     */
+    @Value("${clearing.house.url}")
+    private final @NonNull String clearingHouse;
+
+    /**
+     * The policy negotiation status from application.properties.
+     */
+    @Value("${policy.negotiation}")
+    private final @NonNull boolean policyNegotiation;
+
+    /**
+     * Setting for allowing unsupported patterns from application.properties.
+     */
+    @Value("${policy.allow-unsupported-patterns}")
+    private final @NonNull boolean allowUnsupported;
+
+    /**
+     * Usage control framework from application.properties.
+     */
+    @Value("${policy.framework}")
+    private final @NonNull UsageControlFramework ucFramework;
+}
