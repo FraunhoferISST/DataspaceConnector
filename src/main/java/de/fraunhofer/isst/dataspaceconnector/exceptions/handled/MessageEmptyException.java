e708b2d37e58b70fe58affe7ee0a0a6bd2b3e0ba
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-03-06 21:09:22 +0100

    chore: add new exception classes for policy handling

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/exceptions/handled/MessageEmptyException.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/exceptions/handled/MessageEmptyException.java
@@ -1,4 +1,4 @@
-package de.fraunhofer.isst.dataspaceconnector.exceptions.handler;
+package de.fraunhofer.isst.dataspaceconnector.exceptions.handled;
 
 import de.fraunhofer.isst.dataspaceconnector.exceptions.MessageException;
 
