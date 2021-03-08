074312e0eef78ceecf8e48d55065b6a82311a38d
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-08 10:01:25 +0100

    tests: upgrade ContractRulePersistenceTest to JUnit5

--- src/test/java/de/fraunhofer/isst/dataspaceconnector/model/ContractRulePersistenceTest.java
+++ src/test/java/de/fraunhofer/isst/dataspaceconnector/model/ContractRulePersistenceTest.java
@@ -1,25 +1,24 @@
 package de.fraunhofer.isst.dataspaceconnector.model;
 
-import de.fraunhofer.isst.dataspaceconnector.configuration.DatabaseTestsConfig;
-import de.fraunhofer.isst.dataspaceconnector.repositories.RuleRepository;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
 import javax.transaction.Transactional;
 
-@RunWith(SpringRunner.class)
+import de.fraunhofer.isst.dataspaceconnector.configuration.DatabaseTestsConfig;
+import de.fraunhofer.isst.dataspaceconnector.repositories.RuleRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest(classes = {DatabaseTestsConfig.class})
 public class ContractRulePersistenceTest {
 
     @Autowired
     private RuleRepository ruleRepository;
 
-    @Before
+    @BeforeEach
     public void init() {
         ruleRepository.findAll().forEach(r -> ruleRepository.delete(r));
     }
@@ -28,7 +27,7 @@ public class ContractRulePersistenceTest {
     @Test
     public void createContractRule_returnSameContractRule() {
         /*ARRANGE*/
-        Assert.assertTrue(ruleRepository.findAll().isEmpty());
+        assertTrue(ruleRepository.findAll().isEmpty());
 
         ContractRule original = getContractRule();
 
@@ -37,20 +36,20 @@ public class ContractRulePersistenceTest {
         ContractRule persisted = ruleRepository.getOne(original.getId());
 
         /*ASSERT*/
-        Assert.assertEquals(1, ruleRepository.findAll().size());
-        Assert.assertEquals(original, persisted);
+        assertEquals(1, ruleRepository.findAll().size());
+        assertEquals(original, persisted);
     }
 
     @Transactional
     @Test
     public void updateContractRule_newTitle_returnUpdatedContractRule() {
         /*ARRANGE*/
-        Assert.assertTrue(ruleRepository.findAll().isEmpty());
+        assertTrue(ruleRepository.findAll().isEmpty());
 
         ContractRule original = ruleRepository.save(getContractRule());
         String newTitle = "new title";
 
-        Assert.assertEquals(1, ruleRepository.findAll().size());
+        assertEquals(1, ruleRepository.findAll().size());
 
         /*ACT*/
         original.setTitle(newTitle);
@@ -58,25 +57,25 @@ public class ContractRulePersistenceTest {
         ContractRule updated = ruleRepository.getOne(original.getId());
 
         /*ASSERT*/
-        Assert.assertEquals(1, ruleRepository.findAll().size());
-        Assert.assertEquals(newTitle, updated.getTitle());
-        Assert.assertEquals(original.getValue(), updated.getValue());
+        assertEquals(1, ruleRepository.findAll().size());
+        assertEquals(newTitle, updated.getTitle());
+        assertEquals(original.getValue(), updated.getValue());
     }
 
     @Test
     public void deleteContractRule_contractRuleDeleted() {
         /*ARRANGE*/
-        Assert.assertTrue(ruleRepository.findAll().isEmpty());
+        assertTrue(ruleRepository.findAll().isEmpty());
 
         ContractRule contractRule = ruleRepository.save(getContractRule());
 
-        Assert.assertEquals(1, ruleRepository.findAll().size());
+        assertEquals(1, ruleRepository.findAll().size());
 
         /*ACT*/
         ruleRepository.delete(contractRule);
 
         /*ASSERT*/
-        Assert.assertTrue(ruleRepository.findAll().isEmpty());
+        assertTrue(ruleRepository.findAll().isEmpty());
     }
 
     private ContractRule getContractRule() {
