f1bbd33c6c0643e50da942a6e93422c9b3e430c7
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-03-11 19:35:12 +0100

    fix: add service annotation

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/services/IdsEntityResolver.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/IdsEntityResolver.java
@@ -2,10 +2,15 @@ package de.fraunhofer.isst.dataspaceconnector.services;
 
 import de.fraunhofer.iais.eis.ManagedEntity;
 import de.fraunhofer.isst.dataspaceconnector.model.AbstractEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class IdsEntityResolver {
 
     public ManagedEntity getEntityAsIdsObject(final AbstractEntity entity) {
+        // IdsViewer.create(entity)
+        //
+
         // TODO
         return null;
     }
