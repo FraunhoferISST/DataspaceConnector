4288d5e19e56f2a8b28e7a2bfe32634274dd86c4
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-03-12 19:25:29 +0100

    refactor: change MessageExceptionHandler to service class

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/services/messages/MessageExceptionHandler.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/messages/MessageExceptionHandler.java
@@ -3,25 +3,26 @@ package de.fraunhofer.isst.dataspaceconnector.services.messages;
 import de.fraunhofer.iais.eis.RejectionReason;
 import de.fraunhofer.iais.eis.util.ConstraintViolationException;
 import de.fraunhofer.isst.dataspaceconnector.exceptions.InvalidResourceException;
+import de.fraunhofer.isst.dataspaceconnector.exceptions.controller.ResourceNotFoundException;
 import de.fraunhofer.isst.dataspaceconnector.exceptions.handled.InfoModelVersionNotSupportedException;
 import de.fraunhofer.isst.dataspaceconnector.exceptions.handled.MessageEmptyException;
-import de.fraunhofer.isst.dataspaceconnector.exceptions.handled.MessageResponseBuilderException;
 import de.fraunhofer.isst.dataspaceconnector.exceptions.handled.PolicyRestrictionOnDataProvisionException;
 import de.fraunhofer.isst.dataspaceconnector.exceptions.handled.ResponseMessageBuilderException;
-import de.fraunhofer.isst.ids.framework.configuration.ConfigurationContainer;
+import de.fraunhofer.isst.dataspaceconnector.services.ConfigurationService;
 import de.fraunhofer.isst.ids.framework.messaging.model.responses.ErrorResponse;
 import de.fraunhofer.isst.ids.framework.messaging.model.responses.MessageResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
 
 /**
  * This class handles exceptions of type {@link MessageEmptyException}.
  */
-@ControllerAdvice
+@Service
 @RequiredArgsConstructor
 public class MessageExceptionHandler {
 
@@ -31,55 +32,37 @@ public class MessageExceptionHandler {
     public static final Logger LOGGER = LoggerFactory.getLogger(MessageExceptionHandler.class);
 
     /**
-     * The current connector configuration.
+     * Service for the current connector configuration.
      */
-    private final @NonNull ConfigurationContainer configurationContainer;
+    private final @NonNull ConfigurationService configService;
 
     /**
      * Handles thrown {@link MessageEmptyException}.
      *
      * @param exception Exception that was thrown when checking if the message is null.
+     * @return A message response.
      */
-    @ExceptionHandler(MessageEmptyException.class)
-    public void handleMessageEmptyException(final MessageEmptyException exception) {
-        LOGGER.warn("Cannot respond when there is no request. [exception=({})]",
+    public MessageResponse handleMessageEmptyException(final MessageEmptyException exception) {
+        LOGGER.debug("Cannot respond when there is no request. [exception=({})]",
                 exception.getMessage());
-        throw new IllegalArgumentException(exception.getMessage());
+        return ErrorResponse.withDefaultHeader(RejectionReason.BAD_PARAMETERS,
+                exception.getMessage(), configService.extractIdFromConnector(),
+                configService.extractOutboundModelVersionFromConnector());
     }
 
     /**
      * Handles thrown {@link InfoModelVersionNotSupportedException}.
      *
      * @param exception Exception that was thrown when checking the Infomodel version.
+     * @param version Infomodel version of incoming message.
      * @return A message response.
      */
-    @ExceptionHandler(InfoModelVersionNotSupportedException.class)
-    public MessageResponse handleInfoModelNotSupportedException(final InfoModelVersionNotSupportedException exception) {
-        // Get a local copy of the current connector.
-        var connector = configurationContainer.getConnector();
-
+    public MessageResponse handleInfoModelNotSupportedException(final InfoModelVersionNotSupportedException exception, final String version) {
         LOGGER.debug("Information Model version of requesting connector is not supported. "
-                + "[exception=({})]", exception.getMessage());
+                + "[version=({}), exception=({})]", version, exception.getMessage());
         return ErrorResponse.withDefaultHeader(RejectionReason.VERSION_NOT_SUPPORTED,
-                exception.getMessage(), connector.getId(), connector.getOutboundModelVersion());
-    }
-
-    /**
-     * Handles thrown {@link MessageResponseBuilderException}.
-     *
-     * @param exception Exception that was thrown when building a response in a message handler.
-     * @return A message response.
-     */
-    @ExceptionHandler(MessageResponseBuilderException.class)
-    public MessageResponse handleMessageResponseBuilderException(final MessageResponseBuilderException exception) {
-        // Get a local copy of the current connector.
-        var connector = configurationContainer.getConnector();
-
-        LOGGER.debug("IDS response message could not be constructed. [exception=({})]",
-                exception.getMessage());
-        return ErrorResponse.withDefaultHeader(RejectionReason.INTERNAL_RECIPIENT_ERROR,
-                "Response could not be constructed.", connector.getId(),
-                connector.getOutboundModelVersion());
+                exception.getMessage(), configService.extractIdFromConnector(),
+                configService.extractOutboundModelVersionFromConnector());
     }
 
     /**
@@ -88,15 +71,12 @@ public class MessageExceptionHandler {
      * @param exception Exception that was thrown when checking for data access.
      * @return A message response.
      */
-    @ExceptionHandler(PolicyRestrictionOnDataProvisionException.class)
     public MessageResponse handlePolicyRestrictionOnDataProvisionException(final PolicyRestrictionOnDataProvisionException exception) {
-        // Get a local copy of the current connector.
-        final var connector = configurationContainer.getConnector();
-
         LOGGER.debug("Policy restriction detected. [exception=({})]", exception.getMessage());
         return ErrorResponse.withDefaultHeader(RejectionReason.NOT_AUTHORIZED,
                 "Policy restriction detected." + exception.getMessage(),
-                connector.getId(), connector.getOutboundModelVersion());
+                configService.extractIdFromConnector(),
+                configService.extractOutboundModelVersionFromConnector());
     }
 
     /**
@@ -105,15 +85,13 @@ public class MessageExceptionHandler {
      * @param exception Exception that was thrown when converting an ids object to a rdf string.
      * @return A message response.
      */
-    @ExceptionHandler(ConstraintViolationException.class)
     public MessageResponse handleConstraintViolationException(final ConstraintViolationException exception) {
-        // Get a local copy of the current connector.
-        final var connector = configurationContainer.getConnector();
-
-        LOGGER.warn("Failed to convert ids object to string. [exception=({})]", exception.getMessage());
+        LOGGER.debug("IDS response message could not be constructed. [exception=({})]",
+                exception.getMessage());
         return ErrorResponse.withDefaultHeader(RejectionReason.INTERNAL_RECIPIENT_ERROR,
                 "Response could not be constructed.",
-                connector.getId(), connector.getOutboundModelVersion());
+                configService.extractIdFromConnector(),
+                configService.extractOutboundModelVersionFromConnector());
     }
 
     /**
@@ -122,31 +100,85 @@ public class MessageExceptionHandler {
      * @param exception Exception that was thrown when building the response message.
      * @return A message response.
      */
-    @ExceptionHandler(ResponseMessageBuilderException.class)
-    public MessageResponse handleConstraintViolationException(final ResponseMessageBuilderException exception) {
-        // Get a local copy of the current connector.
-        final var connector = configurationContainer.getConnector();
-
+    public MessageResponse handleResponseMessageBuilderException(final ResponseMessageBuilderException exception) {
         LOGGER.warn("Failed to convert ids object to string. [exception=({})]", exception.getMessage());
         return ErrorResponse.withDefaultHeader(RejectionReason.INTERNAL_RECIPIENT_ERROR,
                 "Response could not be constructed.",
-                connector.getId(), connector.getOutboundModelVersion());
+                configService.extractIdFromConnector(),
+                configService.extractOutboundModelVersionFromConnector());
+    }
+
+    /**
+     * Handles thrown {@link IllegalStateException}.
+     *
+     * @param exception Exception that was thrown when trying to send the message.
+     * @return A message response.
+     */
+    public MessageResponse handleIllegalStateException(final IllegalStateException exception) {
+        LOGGER.debug("Wrong ids message type as response. [exception=({})]", exception.getMessage());
+        return ErrorResponse.withDefaultHeader(RejectionReason.INTERNAL_RECIPIENT_ERROR,
+                "Internal server error.", configService.extractIdFromConnector(),
+                configService.extractOutboundModelVersionFromConnector());
+    }
+
+    /**
+     * Handles thrown {@link ResourceNotFoundException}.
+     *
+     * @param exception Exception that was thrown when trying to send the message.
+     * @param requestedElement The requested element.
+     * @param issuerConnector  The issuer connector extracted from the incoming message.
+     * @param messageId        The message id of the incoming message.
+     * @return A message response.
+     */
+    public MessageResponse handleResourceNotFoundException(final ResourceNotFoundException exception,
+                                                           final URI requestedElement,
+                                                           final URI issuerConnector,
+                                                           final URI messageId) {
+        LOGGER.debug("Element could not be found. [exception=({}), resourceId=({}), issuer=({}), messageId=({})]",
+                exception.getMessage(), requestedElement, issuerConnector, messageId);
+        return ErrorResponse.withDefaultHeader(RejectionReason.NOT_FOUND, String.format(
+                "The requested element %s could not be found.", requestedElement),
+                configService.extractIdFromConnector(),
+                configService.extractOutboundModelVersionFromConnector());
+    }
+
+    /**
+     * Build a message response telling the requester that the element could not be found.
+     *
+     * @param requestedElement The requested element.
+     * @param issuerConnector  The issuer connector extracted from the incoming message.
+     * @param messageId        The message id of the incoming message.
+     * @return A message response.
+     */
+    public MessageResponse handleResourceNotFoundException(final URI requestedElement,
+                                                           final URI issuerConnector,
+                                                           final URI messageId) {
+        LOGGER.debug("Element not found. [resourceId=({}), issuer=({}), messageId=({})]",
+                requestedElement, issuerConnector, messageId);
+        return ErrorResponse.withDefaultHeader(RejectionReason.NOT_FOUND, String.format(
+                "The requested element %s could not be found.", requestedElement),
+                configService.extractIdFromConnector(),
+                configService.extractOutboundModelVersionFromConnector());
     }
 
     /**
      * Handles thrown {@link InvalidResourceException}.
      *
      * @param exception Exception that was thrown when building the response message.
+     * @param requestedElement The requested element.
+     * @param issuerConnector  The issuer connector extracted from the incoming message.
+     * @param messageId        The message id of the incoming message.
      * @return A message response.
      */
-    @ExceptionHandler(InvalidResourceException.class)
-    public MessageResponse handleInvalidResourceException(final InvalidResourceException exception) {
-        // Get a local copy of the current connector.
-        final var connector = configurationContainer.getConnector();
-
-        LOGGER.debug("Resource is not in a valid format. exception=({})]", exception.getMessage());
-        return ErrorResponse.withDefaultHeader(RejectionReason.INTERNAL_RECIPIENT_ERROR,
-                "Requested element malformed.", connector.getId(),
-                connector.getOutboundModelVersion());
+    public MessageResponse handleInvalidResourceException(final InvalidResourceException exception,
+                                                          final URI requestedElement,
+                                                          final URI issuerConnector,
+                                                          final URI messageId) {
+        LOGGER.debug("Element not found. [exception=({}), resourceId=({}), issuer=({}), messageId=({})]",
+                exception.getMessage(), requestedElement, issuerConnector, messageId);
+        return ErrorResponse.withDefaultHeader(RejectionReason.NOT_FOUND, String.format(
+                "The requested element %s could not be found.", requestedElement),
+                configService.extractIdFromConnector(),
+                configService.extractOutboundModelVersionFromConnector());
     }
 }
