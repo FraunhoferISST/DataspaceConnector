33e167e7b654b2ec199928a451d25a9544ddfeff
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-03-12 15:14:36 +0100

    docs: add javadocs to MessageUtils methods

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/utils/MessageUtils.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/utils/MessageUtils.java
@@ -7,14 +7,32 @@ import java.net.URI;
 
 public final class MessageUtils {
 
+    /**
+     * Extract requested element from ids description request message.
+     *
+     * @param message The incoming ids message.
+     * @return The id of the requested element.
+     */
     public static URI extractRequestedElementFromMessage(final DescriptionRequestMessage message) {
         return message.getRequestedElement();
     }
 
+    /**
+     * Extract issuer connector from incoming ids message.
+     *
+     * @param message The incoming ids message.
+     * @return The issuer connector of an ids message.
+     */
     public static URI extractIssuerConnectorFromMessage(final Message message) {
         return message.getIssuerConnector();
     }
 
+    /**
+     * Extract the ids of an ids message.
+     *
+     * @param message The incoming ids message.
+     * @return The ids of an ids message.
+     */
     public static URI extractMessageIdFromMessage(final Message message) {
         return message.getId();
     }
