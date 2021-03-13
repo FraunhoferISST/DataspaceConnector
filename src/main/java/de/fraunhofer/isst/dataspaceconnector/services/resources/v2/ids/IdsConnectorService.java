31b9eb4acdffed09c4a665108bc592c9dd730195
jpampus <julia.pampus@isst.fraunhofer.de>
2021-03-13 12:39:02 +0100

    chore: methods for getting the connector with all resources or only offered resources

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/v2/ids/IdsConnectorService.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/v2/ids/IdsConnectorService.java
@@ -5,7 +5,6 @@ import de.fraunhofer.iais.eis.BaseConnectorImpl;
 import de.fraunhofer.iais.eis.Resource;
 import de.fraunhofer.iais.eis.ResourceCatalogBuilder;
 import de.fraunhofer.iais.eis.util.Util;
-import de.fraunhofer.isst.dataspaceconnector.services.messages.MessageExceptionHandler;
 import de.fraunhofer.isst.ids.framework.configuration.ConfigurationContainer;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -29,7 +28,6 @@ public class IdsConnectorService {
 
     /**
      * Build a base connector object with all offered resources.
-     * NOTE: Exceptions are handled automatically by {@link MessageExceptionHandler}.
      *
      * @return The ids base connector object.
      */
@@ -40,8 +38,42 @@ public class IdsConnectorService {
         // Create a connector with a list of offered resources.
         final var connectorImpl = (BaseConnectorImpl) connector;
         connectorImpl.setResourceCatalog(Util.asList(new ResourceCatalogBuilder()
-                ._offeredResource_((ArrayList<? extends Resource>) resourceService.getAllOfferedResources())
+                ._offeredResource_((ArrayList<? extends Resource>)
+                        resourceService.getAllOfferedResources())
                 .build()));
+        return connectorImpl;
+    }
+
+    /**
+     * Build a base connector object with all offered and requested resources.
+     *
+     * @return The ids base connector object.
+     */
+    public BaseConnector getConnectorWithAllResources() {
+        // Get a local copy of the current connector.
+        final var connector = configContainer.getConnector();
+
+        // Create a connector with a list of offered resources.
+        final var connectorImpl = (BaseConnectorImpl) connector;
+        connectorImpl.setResourceCatalog(Util.asList(new ResourceCatalogBuilder()
+                ._offeredResource_((ArrayList<? extends Resource>) resourceService.getAllOfferedResources())
+                ._requestedResource_((ArrayList<? extends Resource>) resourceService.getAllRequestedResources())
+                .build()));
+        return connectorImpl;
+    }
+
+    /**
+     * Build a base connector object without resources.
+     *
+     * @return The ids base connector object.
+     */
+    public BaseConnector getConnectorWithoutResources() {
+        // Get a local copy of the current connector.
+        final var connector = configContainer.getConnector();
+
+        // Create a connector without any resources.
+        final var connectorImpl = (BaseConnectorImpl) connector;
+        connectorImpl.setResourceCatalog(null);
         return connectorImpl;
     }
 }
