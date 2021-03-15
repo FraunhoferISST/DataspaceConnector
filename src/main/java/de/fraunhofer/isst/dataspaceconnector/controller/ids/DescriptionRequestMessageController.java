dea43abcb1babb0b5b7e52186e8e75563bea5a56
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-03-15 21:07:39 +0100

    chore: add DescriptionRequestMessageController

---
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/ids/DescriptionRequestMessageController.java
@@ -0,0 +1,105 @@
+package de.fraunhofer.isst.dataspaceconnector.controller.ids;
+
+import de.fraunhofer.isst.dataspaceconnector.exceptions.MessageException;
+import de.fraunhofer.isst.dataspaceconnector.exceptions.MessageResponseException;
+import de.fraunhofer.isst.dataspaceconnector.model.messages.DescriptionRequestDesc;
+import de.fraunhofer.isst.dataspaceconnector.services.messages.DescriptionRequestService;
+import de.fraunhofer.isst.dataspaceconnector.services.messages.MessageResponseService;
+import de.fraunhofer.isst.dataspaceconnector.utils.ControllerUtils;
+import de.fraunhofer.isst.dataspaceconnector.utils.MessageUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/ids")
+@Tag(name = "IDS Messages", description = "Endpoints for invoke sending IDS messages")
+public class DescriptionRequestMessageController {
+
+    /**
+     * Service for description request messages.
+     */
+    private final @NonNull DescriptionRequestService requestService;
+
+    /**
+     * Service for message responses.
+     */
+    private final @NonNull MessageResponseService responseService;
+
+    /**
+     * Requests metadata from an external connector by building an ArtifactRequestMessage.
+     *
+     * @param recipient The target connector url.
+     * @param elementId The requested element id.
+     * @return OK or error response.
+     */
+    @PostMapping("/request/description")
+    @Operation(summary = "Send ids description request message")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")})
+    @PreAuthorize("hasPermission(#url, 'rw')")
+    @ResponseBody
+    public ResponseEntity<Object> sendDescriptionRequestMessage(
+            @Parameter(description = "The recipient url.", required = true)
+            @RequestParam("recipient") final URI recipient,
+            @Parameter(description = "The id of the requested resource.")
+            @RequestParam(value = "elementId", required = false) final URI elementId) {
+        final var params = List.of(elementId);
+
+        Map<String, String> response;
+        try {
+            final var desc = new DescriptionRequestDesc(elementId);
+            response = requestService.sendMessage(recipient, desc, "");
+        } catch (MessageException exception) {
+            return ControllerUtils.respondIdsMessageFailed(exception);
+        }
+
+        try {
+            final var validResponse = responseService.isValidDescriptionResponse(response);
+            if (validResponse) {
+                final var payload = MessageUtils.extractPayloadFromMultipartMessage(response);
+
+                // Handle response.
+                if (!elementId.toString().equals("")) {
+                    // Get payload as resource and save it as requested resource.
+                    final var resource = responseService.getResourceFromPayload(payload);
+                    final var uri = responseService.saveMetadata(resource);
+
+                    // Return the uri of the saved resource.
+                    return new ResponseEntity<>(uri, HttpStatus.OK);
+                } else {
+                    // Get payload as component.
+                    final var component = responseService.getComponentFromPayload(payload);
+                    return new ResponseEntity<>(component, HttpStatus.OK);
+                }
+            } else {
+                // If the response is not a description response message, show the response.
+                return responseService.showRejectionMessage(response);
+            }
+        } catch (MessageResponseException exception) {
+            return ControllerUtils.respondReceivedInvalidResponse(exception);
+        } catch (IllegalArgumentException exception) {
+            // If the response is not of type resource or base connector.
+            return ControllerUtils.respondReceivedInvalidResponse(exception);
+        }
+    }
+}
