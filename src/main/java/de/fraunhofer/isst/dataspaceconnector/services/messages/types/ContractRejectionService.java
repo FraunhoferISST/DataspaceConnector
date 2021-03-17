861537a9a1a6e8a1133c6464f486d4e2d5af1c3f
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-03-17 21:52:43 +0100

    chore: add contract rejection message service

---
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/messages/types/ContractRejectionService.java
@@ -0,0 +1,45 @@
+package de.fraunhofer.isst.dataspaceconnector.services.messages.types;
+
+import de.fraunhofer.iais.eis.ContractRejectionMessageBuilder;
+import de.fraunhofer.iais.eis.Message;
+import de.fraunhofer.iais.eis.RejectionReason;
+import de.fraunhofer.iais.eis.util.ConstraintViolationException;
+import de.fraunhofer.iais.eis.util.TypedLiteral;
+import de.fraunhofer.iais.eis.util.Util;
+import de.fraunhofer.isst.dataspaceconnector.model.messages.ContractRejectionMessageDesc;
+import de.fraunhofer.isst.dataspaceconnector.services.messages.AbstractMessageService;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+
+import static de.fraunhofer.isst.ids.framework.util.IDSUtils.getGregorianNow;
+
+/**
+ * Message service for ids contract request messages.
+ */
+@Service
+public final class ContractRejectionService extends AbstractMessageService<ContractRejectionMessageDesc> {
+
+    @Override
+    public Message buildMessage(final URI recipient, final ContractRejectionMessageDesc desc)
+            throws ConstraintViolationException {
+        final var connectorId = getConnectorService().getConnectorId();
+        final var modelVersion = getConnectorService().getOutboundModelVersion();
+        final var token = getConnectorService().getCurrentDat();
+
+        final var correlationMessage = desc.getCorrelationMessage();
+        final var rejectionReason = new TypedLiteral("Contract not accepted.", "en");
+
+        return new ContractRejectionMessageBuilder()
+                ._securityToken_(token)
+                ._correlationMessage_(correlationMessage)
+                ._issued_(getGregorianNow())
+                ._issuerConnector_(connectorId)
+                ._modelVersion_(modelVersion)
+                ._senderAgent_(connectorId)
+                ._recipientConnector_(Util.asList(recipient))
+                ._rejectionReason_(RejectionReason.MALFORMED_MESSAGE)
+                ._contractRejectionReason_(rejectionReason)
+                .build();
+    }
+}
