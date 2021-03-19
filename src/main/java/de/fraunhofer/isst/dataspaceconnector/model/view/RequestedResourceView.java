7e0c37350f2bc395b4a065c6314aead4e86d5e38
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-19 18:08:25 +0100

    refactor: add requestedresource endpoint

---
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/view/RequestedResourceView.java
@@ -0,0 +1,58 @@
+package de.fraunhofer.isst.dataspaceconnector.model.view;
+
+import java.net.URI;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+
+@Getter
+@Setter
+@Relation(collectionRelation = "requested", itemRelation = "resource")
+public class RequestedResourceView extends RepresentationModel<RequestedResourceView> {
+
+    private Date creationDate;
+    private Date modificationDate;
+
+    /**
+     * The title of the resource.
+     */
+    private String title;
+
+    /**
+     * The description of the resource.
+     */
+    private String description;
+
+    /**
+     * The keywords of the resource.
+     */
+    private List<String> keywords;
+
+    /**
+     * The publisher of the resource.
+     */
+    private URI publisher;
+
+    /**
+     * The language of the resource.
+     */
+    private String language;
+
+    /**
+     * The licence of the resource.
+     */
+    private URI licence;
+
+    /**
+     * The version of the resource.
+     */
+    private long version;
+
+
+    private Map<String, String> additional;
+}
---
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/view/RequestedResourceViewAssembler.java
@@ -0,0 +1,33 @@
+package de.fraunhofer.isst.dataspaceconnector.model.view;
+
+import de.fraunhofer.isst.dataspaceconnector.controller.resources.ResourceContracts;
+import de.fraunhofer.isst.dataspaceconnector.controller.resources.ResourceRepresentations;
+import de.fraunhofer.isst.dataspaceconnector.controller.v2.RequestedResourceController;
+import de.fraunhofer.isst.dataspaceconnector.model.RequestedResource;
+import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn;
+
+@Component
+public class RequestedResourceViewAssembler implements RepresentationModelAssembler<RequestedResource, RequestedResourceView> {
+
+    @Override
+    public RequestedResourceView toModel(final RequestedResource entity) {
+        final var modelMapper = new ModelMapper();
+        final var view = modelMapper.map(entity, RequestedResourceView.class);
+
+        final var selfLink = linkTo(RequestedResourceController.class).slash(entity.getId()).withSelfRel();
+        view.add(selfLink);
+
+        final var contractsLink = linkTo(methodOn(ResourceContracts.class).getResource(entity.getId(), null, null, null)).withRel("contracts");
+        view.add(contractsLink);
+
+        final var representationLink = linkTo(methodOn(ResourceRepresentations.class).getResource(entity.getId(), null, null, null)).withRel("representations");
+        view.add(representationLink);
+
+        return view;
+    }
+}
