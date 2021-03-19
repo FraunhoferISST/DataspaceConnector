73b54d0dc328e854b8a95174ee3166082db1c263
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-19 18:41:57 +0100

    docs: add docs to ContractRuleLinker

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/ContractRuleLinker.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/ContractRuleLinker.java
@@ -1,15 +1,19 @@
 package de.fraunhofer.isst.dataspaceconnector.services.resources;
 
-import de.fraunhofer.isst.dataspaceconnector.model.Contract;
-import de.fraunhofer.isst.dataspaceconnector.model.ContractRule;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
-public class ContractRuleLinker extends BaseUniDirectionalLinkerService<
-        Contract, ContractRule, ContractService, RuleService> {
+import de.fraunhofer.isst.dataspaceconnector.model.Contract;
+import de.fraunhofer.isst.dataspaceconnector.model.ContractRule;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
 
+/**
+ * Handles the relation between a contract and its rules.
+ */
+@Service
+@NoArgsConstructor
+public class ContractRuleLinker extends BaseUniDirectionalLinkerService<Contract, ContractRule,
+        ContractService, RuleService> {
     @Override
     protected List<ContractRule> getInternal(final Contract owner) {
         return owner.getRules();
