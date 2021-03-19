df5ae5367e12a08cb08e6a7e7887102b68ea2b67
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-19 18:56:53 +0100

    docs: add docs to all ResourceServices

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/ResourceService.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/ResourceService.java
@@ -1,20 +1,34 @@
 package de.fraunhofer.isst.dataspaceconnector.services.resources;
 
+import org.springframework.stereotype.Service;
+
 import de.fraunhofer.isst.dataspaceconnector.model.OfferedResource;
 import de.fraunhofer.isst.dataspaceconnector.model.OfferedResourceDesc;
 import de.fraunhofer.isst.dataspaceconnector.model.RequestedResource;
 import de.fraunhofer.isst.dataspaceconnector.model.RequestedResourceDesc;
 import de.fraunhofer.isst.dataspaceconnector.model.Resource;
 import de.fraunhofer.isst.dataspaceconnector.model.ResourceDesc;
-import org.springframework.stereotype.Service;
+import lombok.NoArgsConstructor;
 
-public class ResourceService<T extends Resource, D extends ResourceDesc<T>> extends BaseEntityService<T, D> {
-}
+/**
+ * Handles the basic logic for resources.
+ * @param <T> The resource type.
+ * @param <D> The resource description type.
+ */
+@NoArgsConstructor
+public class ResourceService<T extends Resource, D extends ResourceDesc<T>>
+        extends BaseEntityService<T, D> { }
 
+/**
+ * Handles the basic logic for offered resources.
+ */
 @Service
-class OfferedResourceService extends ResourceService<OfferedResource, OfferedResourceDesc> {
-}
+@NoArgsConstructor
+class OfferedResourceService extends ResourceService<OfferedResource, OfferedResourceDesc> { }
 
+/**
+ * Handles the basic logic for requested resources.
+ */
 @Service
-class RequestedResourceService extends ResourceService<RequestedResource, RequestedResourceDesc> {
-}
+@NoArgsConstructor
+class RequestedResourceService extends ResourceService<RequestedResource, RequestedResourceDesc> { }
