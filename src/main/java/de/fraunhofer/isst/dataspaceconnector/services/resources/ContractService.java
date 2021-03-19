c15f0aadc9a0925c5c932de8cc90c96b2d039954
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-19 18:42:50 +0100

    docs: add docs to ContractService

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/ContractService.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/ContractService.java
@@ -1,9 +1,14 @@
 package de.fraunhofer.isst.dataspaceconnector.services.resources;
 
-import de.fraunhofer.isst.dataspaceconnector.model.Contract;
-import de.fraunhofer.isst.dataspaceconnector.model.ContractDesc;
 import org.springframework.stereotype.Service;
 
+import de.fraunhofer.isst.dataspaceconnector.model.Contract;
+import de.fraunhofer.isst.dataspaceconnector.model.ContractDesc;
+import lombok.NoArgsConstructor;
+
+/**
+ * Handles the basic logic for contracts.
+ */
 @Service
-public class ContractService extends BaseEntityService<Contract, ContractDesc> {
-}
+@NoArgsConstructor
+public class ContractService extends BaseEntityService<Contract, ContractDesc> { }
