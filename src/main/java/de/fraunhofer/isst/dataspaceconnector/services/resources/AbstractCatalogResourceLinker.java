99cdf86009376ef2abe9cc39df23b1ddc9a022de
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-19 18:12:36 +0100

    docs: add docs to all CatalogResourceLinker

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/AbstractCatalogResourceLinker.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/AbstractCatalogResourceLinker.java
@@ -6,33 +6,41 @@ import de.fraunhofer.isst.dataspaceconnector.model.Catalog;
 import de.fraunhofer.isst.dataspaceconnector.model.OfferedResource;
 import de.fraunhofer.isst.dataspaceconnector.model.RequestedResource;
 import de.fraunhofer.isst.dataspaceconnector.model.Resource;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * Base class for linking resources to catalogs.
+ * @param <T> The resource type.
+ */
 public abstract class AbstractCatalogResourceLinker<T extends Resource>
         extends BaseUniDirectionalLinkerService<Catalog, T, CatalogService, ResourceService<T, ?>> {
+    /**
+     * Default constructor.
+     */
     protected AbstractCatalogResourceLinker() {
         super();
     }
 }
 
+/**
+ * Links offered resources to a catalog.
+ */
 @Service
+@NoArgsConstructor
 class CatalogOfferedResourceLinker extends AbstractCatalogResourceLinker<OfferedResource> {
-    public CatalogOfferedResourceLinker() {
-        super();
-    }
-
     @Override
     protected List<OfferedResource> getInternal(final Catalog owner) {
         return owner.getOfferedResources();
     }
 }
 
+/**
+ * Links requested resources to a catalog.
+ */
 @Service
+@NoArgsConstructor
 class CatalogRequestedResourceLinker extends AbstractCatalogResourceLinker<RequestedResource> {
-    public CatalogRequestedResourceLinker() {
-        super();
-    }
-
     @Override
     protected List<RequestedResource> getInternal(final Catalog owner) {
         return owner.getRequestedResources();
