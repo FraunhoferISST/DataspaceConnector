2174978538852b3d861aa9612d8c827ef0bf38e1
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-19 18:57:52 +0100

    docs: add docs to RuleService

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/RuleService.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/RuleService.java
@@ -1,9 +1,14 @@
 package de.fraunhofer.isst.dataspaceconnector.services.resources;
 
-import de.fraunhofer.isst.dataspaceconnector.model.ContractRule;
-import de.fraunhofer.isst.dataspaceconnector.model.ContractRuleDesc;
 import org.springframework.stereotype.Service;
 
+import de.fraunhofer.isst.dataspaceconnector.model.ContractRule;
+import de.fraunhofer.isst.dataspaceconnector.model.ContractRuleDesc;
+import lombok.NoArgsConstructor;
+
+/**
+ * Handles the basic logic for contract rules.
+ */
 @Service
-public class RuleService extends BaseEntityService<ContractRule, ContractRuleDesc> {
-}
+@NoArgsConstructor
+public class RuleService extends BaseEntityService<ContractRule, ContractRuleDesc> { }
