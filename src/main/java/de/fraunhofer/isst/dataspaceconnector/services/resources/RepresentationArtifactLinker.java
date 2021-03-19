b4fcc7938dd99c2b16a573f362f31dc66baa9574
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-19 18:44:52 +0100

    docs: add docs to RepresentationArtifactLinker

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/RepresentationArtifactLinker.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/RepresentationArtifactLinker.java
@@ -1,16 +1,19 @@
 package de.fraunhofer.isst.dataspaceconnector.services.resources;
 
-import de.fraunhofer.isst.dataspaceconnector.model.Artifact;
-import de.fraunhofer.isst.dataspaceconnector.model.Representation;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
-public class RepresentationArtifactLinker
-        extends BaseUniDirectionalLinkerService<Representation, Artifact,
-        RepresentationService, ArtifactService> {
+import de.fraunhofer.isst.dataspaceconnector.model.Artifact;
+import de.fraunhofer.isst.dataspaceconnector.model.Representation;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
 
+/**
+ * Handles the relation between a representation and its artifacts.
+ */
+@Service
+@NoArgsConstructor
+public class RepresentationArtifactLinker extends BaseUniDirectionalLinkerService<Representation,
+        Artifact, RepresentationService, ArtifactService> {
     @Override
     protected List<Artifact> getInternal(final Representation owner) {
         return owner.getArtifacts();
