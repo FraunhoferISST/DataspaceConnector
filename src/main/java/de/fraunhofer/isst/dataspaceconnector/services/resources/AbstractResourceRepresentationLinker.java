9164338e0c957007fde58c64786e19971b3908b5
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-19 18:25:06 +0100

    docs: add docs to ResourceRepresentationLinker

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/AbstractResourceRepresentationLinker.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/AbstractResourceRepresentationLinker.java
@@ -6,33 +6,39 @@ import de.fraunhofer.isst.dataspaceconnector.model.OfferedResource;
 import de.fraunhofer.isst.dataspaceconnector.model.Representation;
 import de.fraunhofer.isst.dataspaceconnector.model.RequestedResource;
 import de.fraunhofer.isst.dataspaceconnector.model.Resource;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * Handles the relation between a resources and its representations.
+ * @param <T> The resource type.
+ */
+@NoArgsConstructor
 public abstract class AbstractResourceRepresentationLinker<T extends Resource>
         extends BaseUniDirectionalLinkerService<T, Representation, ResourceService<T, ?>,
                 RepresentationService> {
-    protected AbstractResourceRepresentationLinker() {
-        super();
-    }
-
+    /**
+     * Get the list of representations owned by the resource.
+     * @param owner The owner of the representations.
+     * @return The list of owned representations.
+     */
     @Override
     protected List<Representation> getInternal(final Resource owner) {
         return owner.getRepresentations();
     }
 }
 
+/**
+ * Handles the relation between an offered resource and its representations.
+ */
 @Service
-class OfferedResourceRepresentation
-        extends AbstractResourceRepresentationLinker<OfferedResource> {
-    public OfferedResourceRepresentation() {
-        super();
-    }
-}
+@NoArgsConstructor
+class OfferedResourceRepresentation extends AbstractResourceRepresentationLinker<OfferedResource> { }
 
+/**
+ * Handles the relation between a requested resource and its representations.
+ */
 @Service
+@NoArgsConstructor
 class RequestedResourceRepresentation
-        extends AbstractResourceRepresentationLinker<RequestedResource> {
-    public RequestedResourceRepresentation() {
-        super();
-    }
-}
+        extends AbstractResourceRepresentationLinker<RequestedResource> { }
