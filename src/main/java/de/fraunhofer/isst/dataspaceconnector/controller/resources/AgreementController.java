8e4f8d03f3a55e153dd5668ae03750b596aad7d8
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-03-24 11:19:58 +0100

    chore: hide unallowed endpoints in swagger ui

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/resources/AgreementController.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/resources/AgreementController.java
@@ -4,6 +4,7 @@ import de.fraunhofer.isst.dataspaceconnector.model.Agreement;
 import de.fraunhofer.isst.dataspaceconnector.model.AgreementDesc;
 import de.fraunhofer.isst.dataspaceconnector.model.view.AgreementView;
 import de.fraunhofer.isst.dataspaceconnector.services.resources.AgreementService;
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -21,18 +22,21 @@ import java.util.UUID;
 @Tag(name = "Resources", description = "Endpoints for CRUD operations on base resources")
 public class AgreementController extends BaseResourceController<Agreement, AgreementDesc, AgreementView, AgreementService> {
     @Override
+    @Hidden
     @ApiResponses(value = {@ApiResponse(responseCode = "405", description = "Not allowed")})
     public HttpEntity<AgreementView> create(final AgreementDesc desc) {
         return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
     }
 
     @Override
+    @Hidden
     @ApiResponses(value = {@ApiResponse(responseCode = "405", description = "Not allowed")})
     public HttpEntity<Object> update(@Valid final UUID resourceId, final AgreementDesc desc) {
         return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
     }
 
     @Override
+    @Hidden
     @ApiResponses(value = {@ApiResponse(responseCode = "405", description = "Not allowed")})
     public ResponseEntity<Void> delete(@Valid final UUID resourceId) {
         return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
