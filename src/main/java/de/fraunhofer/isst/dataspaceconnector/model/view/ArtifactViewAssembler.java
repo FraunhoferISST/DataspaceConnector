2834ca3f0eb459933c51fc2b5dfa96cce1fa4072
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-25 08:22:27 +0100

    docs: document ArtifactViewAssembler

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/view/ArtifactViewAssembler.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/view/ArtifactViewAssembler.java
@@ -3,6 +3,7 @@ package de.fraunhofer.isst.dataspaceconnector.model.view;
 import de.fraunhofer.isst.dataspaceconnector.controller.resources.ArtifactController;
 import de.fraunhofer.isst.dataspaceconnector.controller.resources.RelationshipControllers;
 import de.fraunhofer.isst.dataspaceconnector.model.Artifact;
+import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
@@ -10,22 +11,33 @@ import org.springframework.stereotype.Component;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+/**
+ * Constructor the REST resource for an artifact.
+ */
 @Component
+@NoArgsConstructor
 public class ArtifactViewAssembler implements RepresentationModelAssembler<Artifact, ArtifactView> {
-
+    /**
+     * Construct the ArtifactView from an Artifact.
+     * @param artifact The artifact.
+     * @return The new view.
+     */
     @Override
-    public ArtifactView toModel(final Artifact entity) {
+    public ArtifactView toModel(final Artifact artifact) {
         final var modelMapper = new ModelMapper();
-        final var view = modelMapper.map(entity, ArtifactView.class);
+        final var view = modelMapper.map(artifact, ArtifactView.class);
 
-        final var selfLink = linkTo(ArtifactController.class).slash(entity.getId()).withSelfRel();
+        final var selfLink = linkTo(ArtifactController.class).slash(artifact.getId()).withSelfRel();
         view.add(selfLink);
 
-        final var dataLink = linkTo(methodOn(ArtifactController.class).getData(entity.getId())).withRel("data");
+        final var dataLink = linkTo(methodOn(ArtifactController.class).getData(artifact.getId()))
+                                     .withRel("data");
         view.add(dataLink);
 
-        final var representationLink = linkTo(RelationshipControllers.ArtifactsToRepresentations.class).slash(entity.getId()).withRel("representations");
-        view.add(representationLink);
+        final var repLink = linkTo(RelationshipControllers.ArtifactsToRepresentations.class)
+                                    .slash(artifact.getId())
+                                    .withRel("representations");
+        view.add(repLink);
 
         return view;
     }
