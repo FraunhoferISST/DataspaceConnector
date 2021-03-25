147455ac759721d95671e20678d5b7a08d971ab6
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-25 08:28:16 +0100

    docs: add docs to ContractRuleViewAssembler

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/view/ContractRuleViewAssembler.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/view/ContractRuleViewAssembler.java
@@ -3,24 +3,36 @@ package de.fraunhofer.isst.dataspaceconnector.model.view;
 import de.fraunhofer.isst.dataspaceconnector.controller.resources.RelationshipControllers;
 import de.fraunhofer.isst.dataspaceconnector.controller.resources.RuleController;
 import de.fraunhofer.isst.dataspaceconnector.model.ContractRule;
+import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
+/**
+ * Constructor the REST resource for an contract rule.
+ */
 @Component
-public class ContractRuleViewAssembler implements RepresentationModelAssembler<ContractRule, ContractRuleView> {
-
+@NoArgsConstructor
+public class ContractRuleViewAssembler
+        implements RepresentationModelAssembler<ContractRule, ContractRuleView> {
+    /**
+     * Construct the ContractRuleView from a ContractRule.
+     * @param rule The contract rule.
+     * @return The new view.
+     */
     @Override
-    public ContractRuleView toModel(final ContractRule entity) {
+    public ContractRuleView toModel(final ContractRule rule) {
         final var modelMapper = new ModelMapper();
-        final var view = modelMapper.map(entity, ContractRuleView.class);
+        final var view = modelMapper.map(rule, ContractRuleView.class);
 
-        final var selfLink = linkTo(RuleController.class).slash(entity.getId()).withSelfRel();
+        final var selfLink = linkTo(RuleController.class).slash(rule.getId()).withSelfRel();
         view.add(selfLink);
 
-        final var contractLink = linkTo(RelationshipControllers.RulesToContracts.class).slash(entity.getId()).withRel("contracts");
+        final var contractLink = linkTo(RelationshipControllers.RulesToContracts.class)
+                                         .slash(rule.getId())
+                                         .withRel("contracts");
         view.add(contractLink);
 
         return view;
