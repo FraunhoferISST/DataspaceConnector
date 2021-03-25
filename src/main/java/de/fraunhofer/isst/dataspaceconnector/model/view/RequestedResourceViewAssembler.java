93de0155cc8704d942eafee45c60dae213ffc0fa
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-25 08:40:27 +0100

    docs: add docs to RequestedResourceViewAssembler

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/view/RequestedResourceViewAssembler.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/view/RequestedResourceViewAssembler.java
@@ -1,9 +1,9 @@
 package de.fraunhofer.isst.dataspaceconnector.model.view;
 
 import de.fraunhofer.isst.dataspaceconnector.controller.resources.RelationshipControllers;
+import de.fraunhofer.isst.dataspaceconnector.controller.resources.RequestedResourceController;
 import de.fraunhofer.isst.dataspaceconnector.controller.resources.ResourceContracts;
 import de.fraunhofer.isst.dataspaceconnector.controller.resources.ResourceRepresentations;
-import de.fraunhofer.isst.dataspaceconnector.controller.resources.RequestedResourceController;
 import de.fraunhofer.isst.dataspaceconnector.model.RequestedResource;
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
@@ -12,24 +12,41 @@ import org.springframework.stereotype.Component;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn;
 
+/**
+ * Constructor the REST resource for a requested resource.
+ */
 @Component
-public class RequestedResourceViewAssembler implements RepresentationModelAssembler<RequestedResource, RequestedResourceView> {
-
+public class RequestedResourceViewAssembler
+        implements RepresentationModelAssembler<RequestedResource, RequestedResourceView> {
+    /**
+     * Construct the RequestedResourceView from a RequestedResource.
+     * @param resource The resource.
+     * @return The new view.
+     */
     @Override
-    public RequestedResourceView toModel(final RequestedResource entity) {
+    public RequestedResourceView toModel(final RequestedResource resource) {
         final var modelMapper = new ModelMapper();
-        final var view = modelMapper.map(entity, RequestedResourceView.class);
+        final var view = modelMapper.map(resource, RequestedResourceView.class);
 
-        final var selfLink = linkTo(RequestedResourceController.class).slash(entity.getId()).withSelfRel();
+        final var selfLink =
+                linkTo(RequestedResourceController.class).slash(resource.getId()).withSelfRel();
         view.add(selfLink);
 
-        final var contractsLink = linkTo(methodOn(ResourceContracts.class).getResource(entity.getId(), null, null, null)).withRel("contracts");
+        final var contractsLink = linkTo(
+                methodOn(ResourceContracts.class).getResource(resource.getId(), null, null, null))
+                                          .withRel("contracts");
         view.add(contractsLink);
 
-        final var representationLink = linkTo(methodOn(ResourceRepresentations.class).getResource(entity.getId(), null, null, null)).withRel("representations");
+        final var representationLink =
+                linkTo(methodOn(ResourceRepresentations.class)
+                                .getResource(resource.getId(), null, null, null))
+                        .withRel("representations");
         view.add(representationLink);
 
-        final var catalogLink = linkTo(methodOn(RelationshipControllers.RequestedResourcesToCatalogs.class).getResource(entity.getId(), null, null, null)).withRel("catalogs");
+        final var catalogLink =
+                linkTo(methodOn(RelationshipControllers.RequestedResourcesToCatalogs.class)
+                                .getResource(resource.getId(), null, null, null))
+                        .withRel("catalogs");
         view.add(catalogLink);
 
         return view;
