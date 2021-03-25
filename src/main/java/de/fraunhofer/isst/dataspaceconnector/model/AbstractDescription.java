7ca399687b139069e52ed0c6d4b729ad0f79405c
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-25 09:15:04 +0100

    fix: remove the static as input

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/AbstractDescription.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/AbstractDescription.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 /**
@@ -17,6 +18,7 @@ public class AbstractDescription<T> {
     /**
      * The static id assigned to public endpoints.
      */
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private UUID staticId;
 
     /**
