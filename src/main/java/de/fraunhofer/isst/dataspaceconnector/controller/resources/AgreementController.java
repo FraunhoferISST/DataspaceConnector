139ca4e3bdd5b765e7523dac1c55ca05817f996a
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-25 08:59:15 +0100

    refactor: change the response type of the Controller

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/resources/AgreementController.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/resources/AgreementController.java
@@ -1,5 +1,8 @@
 package de.fraunhofer.isst.dataspaceconnector.controller.resources;
 
+import javax.validation.Valid;
+import java.util.UUID;
+
 import de.fraunhofer.isst.dataspaceconnector.model.Agreement;
 import de.fraunhofer.isst.dataspaceconnector.model.AgreementDesc;
 import de.fraunhofer.isst.dataspaceconnector.model.view.AgreementView;
@@ -8,14 +11,10 @@ import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/agreements")
@@ -24,14 +23,14 @@ public class AgreementController extends BaseResourceController<Agreement, Agree
     @Override
     @Hidden
     @ApiResponses(value = {@ApiResponse(responseCode = "405", description = "Not allowed")})
-    public HttpEntity<AgreementView> create(final AgreementDesc desc) {
+    public ResponseEntity<AgreementView> create(final AgreementDesc desc) {
         return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
     }
 
     @Override
     @Hidden
     @ApiResponses(value = {@ApiResponse(responseCode = "405", description = "Not allowed")})
-    public HttpEntity<Object> update(@Valid final UUID resourceId, final AgreementDesc desc) {
+    public ResponseEntity<Object> update(@Valid final UUID resourceId, final AgreementDesc desc) {
         return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
     }
 
