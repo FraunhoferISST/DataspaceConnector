370afc40dd0b7bb9f4cd9d77a81b9bea470e8437
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-25 08:50:12 +0100

    docs: add missing docs to AbstractDescription

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/AbstractDescription.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/AbstractDescription.java
@@ -1,12 +1,12 @@
 package de.fraunhofer.isst.dataspaceconnector.model;
 
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Data;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
 
 /**
  * The base class for all descriptions.
@@ -19,13 +19,23 @@ public class AbstractDescription<T> {
      */
     private UUID staticId;
 
+    /**
+     * The overflow for all elements that cannot be mapped.
+     */
     @JsonIgnore
     private Map<String, String> additional;
 
+    /**
+     * Add a value to the overflow field.
+     * If the key already exists it will be overwritten.
+     * @param key The key.
+     * @param value The value.
+     */
     @JsonAnySetter
-    public void set(final String key, final String value) {
-        if(additional == null)
-            additional = new HashMap<String, String>();
+    public void addOverflow(final String key, final String value) {
+        if (additional == null) {
+            additional = new HashMap<>();
+        }
 
         additional.put(key, value);
     }
