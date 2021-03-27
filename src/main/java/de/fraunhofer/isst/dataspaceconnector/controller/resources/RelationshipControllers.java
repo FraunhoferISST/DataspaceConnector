c4d912f2c450ef12e4327587f0bd5772bf9b47d9
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-03-27 19:47:49 +0100

    feat: add GET endpoints for artifact and agreement linker services

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/resources/RelationshipControllers.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/controller/resources/RelationshipControllers.java
@@ -1,20 +1,36 @@
 package de.fraunhofer.isst.dataspaceconnector.controller.resources;
 
+import de.fraunhofer.isst.dataspaceconnector.model.Agreement;
 import de.fraunhofer.isst.dataspaceconnector.model.Artifact;
 import de.fraunhofer.isst.dataspaceconnector.model.Contract;
 import de.fraunhofer.isst.dataspaceconnector.model.ContractRule;
 import de.fraunhofer.isst.dataspaceconnector.model.OfferedResource;
 import de.fraunhofer.isst.dataspaceconnector.model.Representation;
 import de.fraunhofer.isst.dataspaceconnector.model.RequestedResource;
+import de.fraunhofer.isst.dataspaceconnector.model.view.AgreementView;
 import de.fraunhofer.isst.dataspaceconnector.model.view.ArtifactView;
 import de.fraunhofer.isst.dataspaceconnector.model.view.ContractRuleView;
 import de.fraunhofer.isst.dataspaceconnector.model.view.ContractView;
 import de.fraunhofer.isst.dataspaceconnector.model.view.OfferedResourceView;
 import de.fraunhofer.isst.dataspaceconnector.model.view.RepresentationView;
 import de.fraunhofer.isst.dataspaceconnector.services.resources.RelationshipServices;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
 
 public final class RelationshipControllers {
 
@@ -64,5 +80,65 @@ public final class RelationshipControllers {
     @RequestMapping("/api/contracts/{id}/requests")
     @Tag(name = "Contracts", description = "Endpoints for linking contracts to requests")
     public static class ContractsToRequestedResources extends BaseResourceChildController<RelationshipServices.ContractRequestedResourceLinker, Contract, ContractView> {
+    }
+
+    @RestController
+    @RequestMapping("/api/artifacts/{id}/agreements")
+    @Tag(name = "Artifacts", description = "Endpoints for linking artifacts to agreements")
+    public static class ArtifactsToAgreements extends BaseResourceChildController<RelationshipServices.AgreementArtifactLinker, Artifact, ArtifactView> {
+        @Override
+        @Hidden
+        @ApiResponses(value = {@ApiResponse(responseCode = "405", description = "Not allowed")})
+        public HttpEntity<PagedModel<ArtifactView>> addResources(
+                @Valid @PathVariable(name = "id") final UUID ownerId,
+                @Valid @RequestBody final List<URI> resources) {
+            return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
+        }
+
+        @Override
+        @Hidden
+        @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "No content")})
+        public HttpEntity<Void> replaceResources(@Valid @PathVariable(name = "id") final UUID ownerId,
+                                                 @Valid @RequestBody final List<URI> resources) {
+            return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
+        }
+
+        @Override
+        @Hidden
+        @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "No content")})
+        public HttpEntity<Void> removeResources(@Valid @PathVariable(name = "id") final UUID ownerId,
+                                                @Valid @RequestBody final List<URI> resources) {
+            return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
+        }
+    }
+
+    @RestController
+    @RequestMapping("/api/agreements/{id}/artifacts")
+    @Tag(name = "Agreements", description = "Endpoints for linking agreements to artifacts")
+    public static class AgreementsToArtifacts extends BaseResourceChildController<RelationshipServices.ArtifactAgreementLinker, Agreement, AgreementView> {
+        @Override
+        @Hidden
+        @ApiResponses(value = {@ApiResponse(responseCode = "405", description = "Not allowed")})
+        public HttpEntity<PagedModel<AgreementView>> addResources(
+                @Valid @PathVariable(name = "id") final UUID ownerId,
+                @Valid @RequestBody final List<URI> resources) {
+            return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
+        }
+
+        @Override
+        @Hidden
+        @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "No content")})
+        public HttpEntity<Void> replaceResources(@Valid @PathVariable(name = "id") final UUID ownerId,
+                                                 @Valid @RequestBody final List<URI> resources) {
+            return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
+        }
+
+        @Override
+        @Hidden
+        @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "No content")})
+        public HttpEntity<Void> removeResources(@Valid @PathVariable(name = "id") final UUID ownerId,
+                                                @Valid @RequestBody final List<URI> resources) {
+            return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
+        }
     }
 }
