f20dc0b2c6678190749789ca0cb1e5dfbdf96e44
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-03-31 14:12:14 +0200

    fix: agreement view

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/view/AgreementView.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/view/AgreementView.java
@@ -1,18 +1,20 @@
 package de.fraunhofer.isst.dataspaceconnector.model.view;
 
+import java.net.URI;
+import java.util.Date;
+
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
 
-import java.util.Date;
-
 @Getter
 @Setter
-@Relation(collectionRelation = "agreement", itemRelation = "agreement")
+@Relation(collectionRelation = "agreements", itemRelation = "agreement")
 public class AgreementView extends RepresentationModel<AgreementView> {
-    private Date creationDate;
-    private Date modificationDate;
+    private Date   creationDate;
+    private Date   modificationDate;
+    private URI    remoteId;
+    private boolean confirmed;
     private String value;
-    private boolean automatedDownload;
 }
