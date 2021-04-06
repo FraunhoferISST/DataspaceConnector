c9fdfdf706bae2d8c813e21f93f209f88abd5f98
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2021-04-06 13:38:50 +0200

    refactor: refactor EntityDependencyResolver to utilize query in ContractRepository

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/EntityDependencyResolver.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/resources/EntityDependencyResolver.java
@@ -1,12 +1,6 @@
 package de.fraunhofer.isst.dataspaceconnector.services.resources;
 
-import de.fraunhofer.isst.dataspaceconnector.exceptions.ResourceNotFoundException;
-import de.fraunhofer.isst.dataspaceconnector.model.Agreement;
-import de.fraunhofer.isst.dataspaceconnector.model.Artifact;
 import de.fraunhofer.isst.dataspaceconnector.model.Contract;
-import de.fraunhofer.isst.dataspaceconnector.model.ContractRule;
-import de.fraunhofer.isst.dataspaceconnector.model.OfferedResource;
-import de.fraunhofer.isst.dataspaceconnector.model.Representation;
 import de.fraunhofer.isst.dataspaceconnector.utils.EndpointUtils;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,108 +21,19 @@ public class EntityDependencyResolver {
     private static final Logger LOGGER = LoggerFactory.getLogger(EntityDependencyResolver.class);
 
     /**
-     * Service for linking artifact and representations.
+     * Service for persisting and querying contracts.
      */
-    private final @NonNull RelationshipServices.ArtifactRepresentationLinker artifactLinker;
+    private final @NonNull ContractService contractService;
 
     /**
-     * Service for linking representations and offered resources.
-     */
-    private final @NonNull RelationshipServices.RepresentationOfferedResourceLinker representationLinker;
-
-    /**
-     * Service for linking contracts and offered resources.
-     */
-    private final @NonNull OfferedResourceContractLinker resourceLinker;
-
-    /**
-     * Service for linking contracts and rules.
-     */
-    private final @NonNull ContractRuleLinker ruleLinker;
-
-    /**
-     * Service for handling artifacts.
-     */
-    private final @NonNull ArtifactService artifactService;
-
-    /**
-     * Service for linking agreements and artifacts.
-     */
-    private final @NonNull RelationshipServices.AgreementArtifactLinker agreementLinker;
-
-    /**
-     * Iterate over all artifacts, representations, and resources to get all contract offers that
-     * may match a requested artifact.
+     * Gets all contracts applicable for a specific artifact by using the query defined in the
+     * {@link de.fraunhofer.isst.dataspaceconnector.repositories.ContractRepository}.
      *
      * @param artifactId The artifact id.
      * @return List of contract offers.
-     * @throws ResourceNotFoundException If the artifact could not be found.
      */
-    public List<Contract> getContractOffersByArtifactId(final URI artifactId) throws ResourceNotFoundException {
+    public List<Contract> getContractOffersByArtifactId(final URI artifactId) {
         final var uuid = EndpointUtils.getUUIDFromPath(artifactId);
-        final var artifact = artifactService.get(uuid);
-
-        final var contractList = new ArrayList<Contract>();
-        final var representations = getRepresentationsByArtifact(artifact);
-        for (final var representation : representations) {
-            final var resources = getOfferedResourcesByRepresentation(representation);
-
-            for (final var resource : resources) {
-                final var contracts = getContractsByOfferedResource(resource);
-                contractList.addAll(contracts);
-            }
-        }
-
-        return contractList;
-    }
-
-    /**
-     * Get representations by artifact.
-     *
-     * @param artifact The artifact.
-     * @return List of representations.
-     */
-    private List<Representation> getRepresentationsByArtifact(final Artifact artifact) {
-        return artifactLinker.getInternal(artifact);
-    }
-
-    /**
-     * Get resources by representation.
-     *
-     * @param representation The representations.
-     * @return List of offered resources.
-     */
-    private List<OfferedResource> getOfferedResourcesByRepresentation(final Representation representation) {
-        return representationLinker.getInternal(representation);
-    }
-
-    /**
-     * Get contracts by offered resource.
-     *
-     * @param resource The offered resource.
-     * @return List of contract offers.
-     */
-    private List<Contract> getContractsByOfferedResource(final OfferedResource resource) {
-        return resourceLinker.getInternal(resource);
-    }
-
-    /**
-     * Get rules by contract offer.
-     *
-     * @param contract The contract offer.
-     * @return List of rules.
-     */
-    public List<ContractRule> getRulesByContractOffer(final Contract contract) {
-        return ruleLinker.getInternal(contract);
-    }
-
-    /**
-     * Get artifacts by agreement.
-     *
-     * @param agreement The contract agreement.
-     * @return List of artifacts.
-     */
-    public List<Artifact> getArtifactsByAgreement(final Agreement agreement) {
-        return agreementLinker.getInternal(agreement);
+        return contractService.getByArtifactId(uuid);
     }
 }
