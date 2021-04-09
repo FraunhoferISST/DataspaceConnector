ab9983cc113b5bbd11077622a1f3a1fdc3e46766
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-04-09 08:32:29 +0200

    test: rename MainControllerTest to MainControllerIT

--- src/test/java/de/fraunhofer/isst/dataspaceconnector/controller/MainControllerIT.java
+++ src/test/java/de/fraunhofer/isst/dataspaceconnector/controller/MainControllerIT.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class MainControllerTest {
+class MainControllerIT {
 
     @MockBean
     ConnectorService connectorService;
