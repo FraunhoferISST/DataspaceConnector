57c003c773df8fdf4d4f71fb5bd11b13f3b31e4e
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-04-18 22:41:40 +0200

    docs: add javadocs to MetadataUtils

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/utils/MetadataUtils.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/utils/MetadataUtils.java
@@ -13,6 +13,14 @@ public final class MetadataUtils {
         // not used
     }
 
+    /**
+     * Update string.
+     *
+     * @param oldTitle     Old string.
+     * @param newTitle     New string.
+     * @param defaultTitle Default value.
+     * @return Optional with the new value or without a value.
+     */
     public static Optional<String> updateString(final String oldTitle,
                                                 final String newTitle,
                                                 final String defaultTitle) {
@@ -24,8 +32,16 @@ public final class MetadataUtils {
         return Optional.empty();
     }
 
+    /**
+     * Update uri.
+     *
+     * @param oldUri     Old uri.
+     * @param newUri     New uri.
+     * @param defaultUri Default value.
+     * @return Optional with the new value or without a value.
+     */
     public static Optional<URI> updateUri(final URI oldUri, final URI newUri,
-                                                         final URI defaultUri) {
+                                          final URI defaultUri) {
         final var newValue = newUri == null ? defaultUri : newUri;
         if (oldUri == null || !oldUri.equals(newValue)) {
             return Optional.of(newValue);
@@ -34,8 +50,17 @@ public final class MetadataUtils {
         return Optional.empty();
     }
 
+    /**
+     * Update date.
+     *
+     * @param oldDate     Old date.
+     * @param newDate     New date.
+     * @param defaultDate Default value.
+     * @return Optional with the new value or without a value.
+     */
     public static Optional<ZonedDateTime> updateDate(final ZonedDateTime oldDate,
-            final ZonedDateTime newDate, final ZonedDateTime defaultDate) {
+                                                     final ZonedDateTime newDate,
+                                                     final ZonedDateTime defaultDate) {
         final var newValue = newDate == null ? defaultDate : newDate;
         if (oldDate == null || !oldDate.equals(newValue)) {
             return Optional.of(newValue);
@@ -44,6 +69,14 @@ public final class MetadataUtils {
         return Optional.empty();
     }
 
+    /**
+     * Update list of strings.
+     *
+     * @param oldList     Old list.
+     * @param newList     New list.
+     * @param defaultList Default values.
+     * @return Optional with the new value or without a value.
+     */
     public static Optional<List<String>> updateStringList(
             final List<String> oldList,
             final List<String> newList,
@@ -57,8 +90,17 @@ public final class MetadataUtils {
         return Optional.empty();
     }
 
+    /**
+     * Update map of strings.
+     *
+     * @param oldMap     Old map.
+     * @param newMap     New map.
+     * @param defaultMap Default values.
+     * @return Optional with the new value or without a value.
+     */
     public static Optional<Map<String, String>> updateStringMap(final Map<String, String> oldMap,
-            final Map<String, String> newMap, final Map<String, String> defaultMap) {
+                                                                final Map<String, String> newMap,
+                                                                final Map<String, String> defaultMap) {
         // TODO Implement cleaning like in updateStringList
         final var newValues = newMap == null ? defaultMap : newMap;
         if (oldMap == null || !oldMap.equals(newValues)) {
@@ -68,16 +110,35 @@ public final class MetadataUtils {
         return Optional.empty();
     }
 
+    /**
+     * Clean list of strings.
+     *
+     * @param list List of strings.
+     * @return Cleared list.
+     */
     public static List<String> cleanStringList(final List<String> list) {
         var result = removeNullFromList(list);
         result = removeEmptyStringFromList(result);
         return result;
     }
 
+    /**
+     * Remove null values from list.
+     *
+     * @param list List of values.
+     * @param <T>  Class type.
+     * @return List without null values.
+     */
     public static <T> List<T> removeNullFromList(final List<T> list) {
         return list.stream().filter(Objects::nonNull).collect(Collectors.toList());
     }
 
+    /**
+     * Remove empty values from list of strings.
+     *
+     * @param list List of strings.
+     * @return List without empty strings.
+     */
     public static List<String> removeEmptyStringFromList(final List<String> list) {
         return list.stream().filter(x -> !x.isEmpty()).collect(Collectors.toList());
     }
