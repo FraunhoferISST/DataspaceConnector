8f65d818715692de3664a09533ef1cc7c504e384
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-04-18 21:47:27 +0200

    refactor: deserialize all message types in AbstractMessageService

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/services/messages/AbstractMessageService.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/messages/AbstractMessageService.java
@@ -6,15 +6,14 @@ import de.fraunhofer.isst.dataspaceconnector.exceptions.MessageBuilderException;
 import de.fraunhofer.isst.dataspaceconnector.exceptions.MessageException;
 import de.fraunhofer.isst.dataspaceconnector.exceptions.MessageResponseException;
 import de.fraunhofer.isst.dataspaceconnector.model.messages.MessageDesc;
-import de.fraunhofer.isst.dataspaceconnector.services.ids.DeserializationService;
 import de.fraunhofer.isst.dataspaceconnector.services.ids.ConnectorService;
+import de.fraunhofer.isst.dataspaceconnector.services.ids.DeserializationService;
 import de.fraunhofer.isst.dataspaceconnector.utils.ErrorMessages;
 import de.fraunhofer.isst.dataspaceconnector.utils.MessageUtils;
 import de.fraunhofer.isst.ids.framework.communication.http.IDSHttpService;
 import de.fraunhofer.isst.ids.framework.daps.ClaimsException;
+import lombok.extern.log4j.Log4j2;
 import org.apache.commons.fileupload.FileUploadException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
@@ -23,11 +22,8 @@ import java.util.Map;
 /**
  * Abstract class for building, sending, and processing ids messages.
  */
+@Log4j2
 public abstract class AbstractMessageService<D extends MessageDesc> {
-    /**
-     * The logging service.
-     */
-    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractMessageService.class);
 
     /**
      * Service for ids communication.
@@ -77,24 +73,40 @@ public abstract class AbstractMessageService<D extends MessageDesc> {
             final var header = buildMessage(desc);
 
             final var body = MessageUtils.buildIdsMultipartMessage(header, payload);
-            LOGGER.debug("Built request message:" + body); // TODO Add logging house class
+            if (log.isDebugEnabled()) {
+                log.debug("Built request message:" + body); // TODO Add logging house class
+            }
 
             // Send message and return response.
             return idsHttpService.sendAndCheckDat(body, recipient);
         } catch (MessageBuilderException e) {
-            LOGGER.warn("Failed to build ids request message. [exception=({})]", e.getMessage());
+            if (log.isWarnEnabled()) {
+                log.warn("Failed to build ids request message. [exception=({})]",
+                        e.getMessage(), e);
+            }
             throw new MessageException(ErrorMessages.MESSAGE_BUILD_FAILED.toString(), e);
         } catch (MessageResponseException e) {
-            LOGGER.warn("Failed to read ids response message. [exception=({})]", e.getMessage());
+            if (log.isWarnEnabled()) {
+                log.warn("Failed to read ids response message. [exception=({})]",
+                        e.getMessage(), e);
+            }
             throw new MessageException(ErrorMessages.INVALID_RESPONSE.toString(), e);
         } catch (ConstraintViolationException e) {
-            LOGGER.warn("Ids message could not be built. [exception=({})]", e.getMessage());
+            if (log.isWarnEnabled()) {
+                log.warn("Ids message could not be built. [exception=({})]",
+                        e.getMessage(), e);
+            }
             throw new MessageException(ErrorMessages.HEADER_BUILD_FAILED.toString(), e);
         } catch (ClaimsException e) {
-            LOGGER.debug("Invalid DAT in incoming message. [exception=({})]", e.getMessage());
+            if (log.isDebugEnabled()) {
+                log.debug("Invalid DAT in incoming message. [exception=({})]",
+                        e.getMessage(), e);
+            }
             throw new MessageException(ErrorMessages.INVALID_RESPONSE_DAT.toString(), e);
         } catch (FileUploadException | IOException e) {
-            LOGGER.warn("Message could not be sent. [exception=({})]", e.getMessage());
+            if (log.isWarnEnabled()) {
+                log.warn("Message could not be sent. [exception=({})]", e.getMessage(), e);
+            }
             throw new MessageException(ErrorMessages.MESSAGE_NOT_SENT.toString(), e);
         }
     }
@@ -110,17 +122,17 @@ public abstract class AbstractMessageService<D extends MessageDesc> {
         try {
             // MessageResponseException is handled at a higher level.
             final var header = MessageUtils.extractHeaderFromMultipartMessage(message);
-            final var idsMessage = getDeserializer().getResponseMessage(header);
+            final var idsMessage = getDeserializer().getMessage(header);
 
             final var messageType = idsMessage.getClass();
             final var allowedType = getResponseMessageType();
             return messageType.equals(allowedType);
         } catch (MessageResponseException | IllegalArgumentException e) {
-            LOGGER.debug("Failed to read response header. [exception=({})]", e.getMessage());
+            log.debug("Failed to read response header. [exception=({})]", e.getMessage());
             throw new MessageResponseException(ErrorMessages.MALFORMED_HEADER.toString(), e);
         } catch (Exception e) {
             // NOTE: Should not be reached.
-            LOGGER.warn("Something else went wrong. [exception=({})]", e.getMessage());
+            log.warn("Something else went wrong. [exception=({})]", e.getMessage());
             throw new MessageResponseException(ErrorMessages.INVALID_RESPONSE.toString(), e);
         }
     }
@@ -141,14 +153,5 @@ public abstract class AbstractMessageService<D extends MessageDesc> {
      */
     public DeserializationService getDeserializer() {
         return deserializationService;
-    }
-
-    /**
-     * Getter for the logger so that it can be used by subclasses.
-     *
-     * @return The logger
-     */
-    protected Logger getLogger() {
-        return LOGGER;
     }
 }
