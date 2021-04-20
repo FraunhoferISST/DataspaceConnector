52b8f7c8dc870e8a67b24edd321c72aec7f578d1
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-04-20 09:53:36 +0200

    chore: add remote address

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/model/Artifact.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/model/Artifact.java
@@ -1,16 +1,16 @@
 package de.fraunhofer.isst.dataspaceconnector.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Inheritance;
-import javax.persistence.ManyToMany;
-import java.net.URI;
-import java.util.List;
-
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.ManyToMany;
+import java.net.URI;
+import java.util.List;
 
 /**
  * An artifact stores and encapsulates data.
@@ -32,6 +32,11 @@ public abstract class Artifact extends AbstractEntity {
      * The artifact id on provider side.
      */
     private URI remoteId;
+
+    /**
+     * The provider's address for artifact request messages.
+     */
+    private URI remoteAddress;
 
     /**
      * The title of the catalog.
