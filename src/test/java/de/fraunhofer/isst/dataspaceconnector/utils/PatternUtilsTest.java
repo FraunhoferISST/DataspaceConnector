2d4da8e341462e45459aa541515a6726115e77f8
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-04-24 20:21:16 +0200

    test: add test for buildConnectorRestrictedUsageRule

--- src/test/java/de/fraunhofer/isst/dataspaceconnector/utils/PatternUtilsTest.java
+++ src/test/java/de/fraunhofer/isst/dataspaceconnector/utils/PatternUtilsTest.java
@@ -3,12 +3,9 @@ package de.fraunhofer.isst.dataspaceconnector.utils;
 import de.fraunhofer.iais.eis.Action;
 import de.fraunhofer.iais.eis.BinaryOperator;
 import de.fraunhofer.iais.eis.Constraint;
-import de.fraunhofer.iais.eis.ConstraintImpl;
-import de.fraunhofer.iais.eis.Duty;
 import de.fraunhofer.iais.eis.LeftOperand;
 import de.fraunhofer.iais.eis.Permission;
 import de.fraunhofer.iais.eis.Prohibition;
-import de.fraunhofer.iais.eis.Rule;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
@@ -23,41 +20,53 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PatternUtilsTest {
 
-   private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+   private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
    @Test
    public void buildProvideAccessRule_returnPermissionWithoutConstraintsAndPostDuties() {
+       /* ARRANGE */
+       // Nothing to arrange here.
+
        /* ACT */
-       Rule rule = PatternUtils.buildProvideAccessRule();
+       final var rule = PatternUtils.buildProvideAccessRule();
 
        /* ASSERT */
        assertTrue(Permission.class.isAssignableFrom(rule.getClass()));
+       assertEquals(Action.USE, rule.getAction().get(0));
        assertNull(rule.getConstraint());
        assertNull(((Permission) rule).getPostDuty());
    }
 
    @Test
    public void buildProhibitAccessRule_returnProhibition() {
+       /* ARRANGE */
+       // Nothing to arrange here.
+
        /* ACT */
-       Rule rule = PatternUtils.buildProhibitAccessRule();
+       final var rule = PatternUtils.buildProhibitAccessRule();
 
        /* ASSERT */
        assertTrue(Prohibition.class.isAssignableFrom(rule.getClass()));
+       assertEquals(Action.USE, rule.getAction().get(0));
    }
 
    @Test
    public void buildNTimesUsageRule_returnPermissionWithConstraint() {
+       /* ARRANGE */
+       // Nothing to arrange here.
+
        /* ACT */
-       Rule rule = PatternUtils.buildNTimesUsageRule();
+       final var rule = PatternUtils.buildNTimesUsageRule();
 
        /* ASSERT */
        assertTrue(Permission.class.isAssignableFrom(rule.getClass()));
+       assertEquals(Action.USE, rule.getAction().get(0));
        assertEquals(1, rule.getConstraint().size());
 
-       Constraint constraint = (ConstraintImpl) (ConstraintImpl) rule.getConstraint().get(0);
+       final var constraint = (Constraint) rule.getConstraint().get(0);
        assertEquals(LeftOperand.COUNT, constraint.getLeftOperand());
 
-       BinaryOperator operator = constraint.getOperator();
+       final var operator = constraint.getOperator();
        assertTrue((BinaryOperator.LT.equals(operator))
                || (BinaryOperator.LTEQ.equals(operator)));
 
@@ -66,17 +75,21 @@ public class PatternUtilsTest {
 
    @Test
    public void buildDurationUsageRule_returnPermissionWithConstraint() {
+       /* ARRANGE */
+       // Nothing to arrange here.
+
        /* ACT */
-       Rule rule = PatternUtils.buildDurationUsageRule();
+       final var rule = PatternUtils.buildDurationUsageRule();
 
        /* ASSERT */
        assertTrue(Permission.class.isAssignableFrom(rule.getClass()));
+       assertEquals(Action.USE, rule.getAction().get(0));
        assertEquals(1, rule.getConstraint().size());
 
-       Constraint constraint = (ConstraintImpl) (ConstraintImpl) rule.getConstraint().get(0);
+       final var constraint = (Constraint) rule.getConstraint().get(0);
        assertEquals(LeftOperand.ELAPSED_TIME, constraint.getLeftOperand());
 
-       BinaryOperator operator = constraint.getOperator();
+       final var operator = constraint.getOperator();
        assertTrue((BinaryOperator.SHORTER_EQ.equals(operator))
                || (BinaryOperator.SHORTER.equals(operator)));
 
@@ -85,23 +98,27 @@ public class PatternUtilsTest {
 
    @Test
    public void buildIntervalUsageRule_returnPermissionWithTwoConstraints() {
+       /* ARRANGE */
+       // Nothing to arrange here.
+
        /* ACT */
-       Rule rule = PatternUtils.buildIntervalUsageRule();
+       final var rule = PatternUtils.buildIntervalUsageRule();
 
        /* ASSERT */
        assertTrue(Permission.class.isAssignableFrom(rule.getClass()));
+       assertEquals(Action.USE, rule.getAction().get(0));
        assertEquals(2, rule.getConstraint().size());
 
-       Constraint constraint1 = (ConstraintImpl) (ConstraintImpl) rule.getConstraint().get(0);
+       final var constraint1 = (Constraint) rule.getConstraint().get(0);
        assertEquals(LeftOperand.POLICY_EVALUATION_TIME, constraint1.getLeftOperand());
-       BinaryOperator operator1 = constraint1.getOperator();
+       final var operator1 = constraint1.getOperator();
        assertTrue((BinaryOperator.BEFORE.equals(operator1))
                || (BinaryOperator.AFTER.equals(operator1)));
        assertTrue(isValidDate(constraint1.getRightOperand().getValue()));
 
-       Constraint constraint2 = (ConstraintImpl) (ConstraintImpl) rule.getConstraint().get(1);
+       final var constraint2 = (Constraint) rule.getConstraint().get(1);
        assertEquals(LeftOperand.POLICY_EVALUATION_TIME, constraint2.getLeftOperand());
-       BinaryOperator operator2 = constraint2.getOperator();
+       final var operator2 = constraint2.getOperator();
        assertTrue((BinaryOperator.BEFORE.equals(operator2))
                || (BinaryOperator.AFTER.equals(operator2)));
        assertTrue(isValidDate(constraint2.getRightOperand().getValue()));
@@ -109,73 +126,104 @@ public class PatternUtilsTest {
 
    @Test
    public void buildUsageUntilDeletionRule_returnPermissionWithTwoConstraintsAndPostDuty() {
+       /* ARRANGE */
+       // Nothing to arrange here.
+
        /* ACT */
-       Rule rule = PatternUtils.buildUsageUntilDeletionRule();
+       final var rule = PatternUtils.buildUsageUntilDeletionRule();
 
        /* ASSERT */
        assertTrue(Permission.class.isAssignableFrom(rule.getClass()));
+       assertEquals(Action.USE, rule.getAction().get(0));
        assertEquals(2, rule.getConstraint().size());
        assertEquals(1, ((Permission) rule).getPostDuty().size());
 
-       Constraint constraint1 = (ConstraintImpl) rule.getConstraint().get(0);
+       final var constraint1 = (Constraint) rule.getConstraint().get(0);
        assertEquals(LeftOperand.POLICY_EVALUATION_TIME, constraint1.getLeftOperand());
-       BinaryOperator operator1 = constraint1.getOperator();
+       final var operator1 = constraint1.getOperator();
        assertTrue((BinaryOperator.BEFORE.equals(operator1))
                || (BinaryOperator.AFTER.equals(operator1)));
        assertTrue(isValidDate(constraint1.getRightOperand().getValue()));
 
-       Constraint constraint2 = (ConstraintImpl) rule.getConstraint().get(1);
+       final var constraint2 = (Constraint) rule.getConstraint().get(1);
        assertEquals(LeftOperand.POLICY_EVALUATION_TIME, constraint2.getLeftOperand());
-       BinaryOperator operator2 = constraint2.getOperator();
+       final var operator2 = constraint2.getOperator();
        assertTrue((BinaryOperator.BEFORE.equals(operator2))
                || (BinaryOperator.AFTER.equals(operator2)));
        assertTrue(isValidDate(constraint2.getRightOperand().getValue()));
 
-       Duty duty = ((Permission) rule).getPostDuty().get(0);
+       final var duty = ((Permission) rule).getPostDuty().get(0);
        assertEquals(1, duty.getConstraint().size());
 
-       Constraint dutyConstraint = (ConstraintImpl) duty.getConstraint().get(0);
+       final var dutyConstraint = (Constraint) duty.getConstraint().get(0);
        assertEquals(LeftOperand.POLICY_EVALUATION_TIME, dutyConstraint.getLeftOperand());
-       BinaryOperator dutyConstraintOperator = dutyConstraint.getOperator();
+       final var dutyConstraintOperator = dutyConstraint.getOperator();
        assertEquals(BinaryOperator.TEMPORAL_EQUALS, dutyConstraintOperator);
        assertTrue(isValidDate(dutyConstraint.getRightOperand().getValue()));
    }
 
    @Test
    public void buildUsageLoggingRule_returnPermissionWithPostDuty() {
+       /* ARRANGE */
+       // Nothing to arrange here.
+
        /* ACT */
-       Rule rule = PatternUtils.buildUsageLoggingRule();
+       final var rule = PatternUtils.buildUsageLoggingRule();
 
        /* ASSERT */
        assertTrue(Permission.class.isAssignableFrom(rule.getClass()));
+       assertEquals(Action.USE, rule.getAction().get(0));
        assertEquals(1, ((Permission) rule).getPostDuty().size());
 
-       Action dutyAction = ((Permission) rule).getPostDuty().get(0).getAction().get(0);
+       final var dutyAction = ((Permission) rule).getPostDuty().get(0).getAction().get(0);
        assertEquals(Action.LOG, dutyAction);
    }
 
    @Test
    public void buildUsageNotificationRule_returnPermissionWithPostDuty() {
+       /* ARRANGE */
+       // Nothing to arrange here.
+
        /* ACT */
-       Rule rule = PatternUtils.buildUsageNotificationRule();
+       final var rule = PatternUtils.buildUsageNotificationRule();
 
        /* ASSERT */
        assertTrue(Permission.class.isAssignableFrom(rule.getClass()));
+       assertEquals(Action.USE, rule.getAction().get(0));
        assertEquals(1, ((Permission) rule).getPostDuty().size());
 
-       Duty duty = ((Permission) rule).getPostDuty().get(0);
-       Action dutyAction = duty.getAction().get(0);
+       final var duty = ((Permission) rule).getPostDuty().get(0);
+       final var dutyAction = duty.getAction().get(0);
        assertEquals(Action.NOTIFY, dutyAction);
        assertEquals(1, duty.getConstraint().size());
 
-       Constraint dutyConstraint = (ConstraintImpl) duty.getConstraint().get(0);
+       final var dutyConstraint = (Constraint) duty.getConstraint().get(0);
        assertEquals(LeftOperand.ENDPOINT, dutyConstraint.getLeftOperand());
 
-       BinaryOperator dutyConstraintOperator = dutyConstraint.getOperator();
+       final var dutyConstraintOperator = dutyConstraint.getOperator();
        assertEquals(BinaryOperator.DEFINES_AS, dutyConstraintOperator);
 
        assertTrue(isValidUri(dutyConstraint.getRightOperand().getValue()));
    }
+
+    @Test
+    public void buildConnectorRestrictedUsageRule_returnPermissionWithPostDuty() {
+        /* ARRANGE */
+        // Nothing to arrange here.
+
+        /* ACT */
+        final var rule = PatternUtils.buildConnectorRestrictedUsageRule();
+
+        /* ASSERT */
+        assertTrue(Permission.class.isAssignableFrom(rule.getClass()));
+        assertEquals(Action.USE, rule.getAction().get(0));
+        assertEquals(1, rule.getConstraint().size());
+
+        final var constraint = (Constraint) rule.getConstraint().get(0);
+        assertEquals(LeftOperand.SYSTEM, constraint.getLeftOperand());
+        assertEquals(BinaryOperator.SAME_AS, constraint.getOperator());
+        assertTrue(isValidUri(constraint.getRightOperand().getValue()));
+    }
 
    /**************************************************************************
     * Utilities.
