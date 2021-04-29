e507165d55bce5576ca53c28e9a10b2bdafd03e5
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-04-29 14:22:31 +0200

    fix: search only confirmed agreements when searching artifact agreements

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/repositories/ArtifactRepository.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/repositories/ArtifactRepository.java
@@ -35,14 +35,26 @@ public interface ArtifactRepository extends RemoteEntityRepository<Artifact> {
             + "WHERE ag.id = :agreementId")
     List<Artifact> findAllByAgreement(UUID agreementId);
 
+    /**
+     * Search for all agreements signed for requested resources by this connector as consumer.
+     * @param artifactId The artifact.
+     * @return The list of agreement ids.
+     */
     @Query("SELECT ag.remoteId "
             + "FROM Artifact a, Agreement ag "
             + "WHERE a.id = :artifactId "
             + "AND ag.remoteId <> 'aced00057372000c6a6176612e6e65742e555249ac01782e439e49ab0300014c0006737472696e677400124c6a6176612f6c616e672f537472696e673b787074000767656e6573697378' "
             + "AND ag.archived = false "
+            + "AND ag.confirmed = true "
             + "AND ag MEMBER OF a.agreements")
     List<URI> findRemoteOriginAgreements(UUID artifactId);
 
+    /**
+     * Set the artifacts data.
+     * @param artifactId The artifact.
+     * @param checkSum The new CRC32C checksum.
+     * @param size The new size in bytes.
+     */
     @Modifying
     @Query("update Artifact a set a.checkSum=:checkSum, a.byteSize=:size where a.id = :artifactId")
     void setArtifactData(UUID artifactId, long checkSum, long size);
