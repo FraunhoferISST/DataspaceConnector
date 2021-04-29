a4167aff40902ebd1c2041ed51a8b0605ddd8591
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-04-29 17:20:40 +0200

    refactor: update docs, restrict the output of InfomodelUpdater

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/services/InfomodelUpdater.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/services/InfomodelUpdater.java
@@ -1,12 +1,18 @@
 package de.fraunhofer.isst.dataspaceconnector.services;
 
 import de.fraunhofer.isst.dataspaceconnector.exceptions.ResourceNotFoundException;
+import de.fraunhofer.isst.dataspaceconnector.model.AbstractEntity;
 
-public interface InfomodelUpdater<I, O> {
+/**
+ * Updates an DSC object by providing an IDS object.
+ * @param <I> The IDS object type.
+ * @param <O> The DSC object type.
+ */
+public interface InfomodelUpdater<I, O extends AbstractEntity> {
     /**
      * Update an entity that is known to the consumer.
      * @param entity The ids object.
-     * @return true If the entity has been updated.
+     * @return The updated dsc object.
      */
     O update(I entity) throws ResourceNotFoundException;
 }
