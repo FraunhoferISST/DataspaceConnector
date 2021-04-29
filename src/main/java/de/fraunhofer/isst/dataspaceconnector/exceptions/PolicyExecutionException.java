86abf420da3467f30f6be1b6bf679164ed44760b
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-04-29 14:08:01 +0200

    chore: remove unused constructors

--- src/main/java/de/fraunhofer/isst/dataspaceconnector/exceptions/PolicyExecutionException.java
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/exceptions/PolicyExecutionException.java
@@ -11,26 +11,11 @@ public class PolicyExecutionException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
     /**
-     * Constructor without params.
-     */
-    public PolicyExecutionException() { }
-
-    /**
      * Construct a PolicyExecutionException with the specified detail message and cause.
      *
      * @param msg The detail message.
      */
     public PolicyExecutionException(final String msg) {
         super(msg);
-    }
-
-    /**
-     * Construct a PolicyExecutionException with the specified detail message and cause.
-     *
-     * @param msg   The detail message.
-     * @param cause The cause.
-     */
-    public PolicyExecutionException(final String msg, final Throwable cause) {
-        super(msg, cause);
     }
 }
