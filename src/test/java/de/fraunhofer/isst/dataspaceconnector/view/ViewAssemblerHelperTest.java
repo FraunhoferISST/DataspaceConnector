a3b57eb44edced41f1949152fc0e53917f5bbec5
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2021-05-05 10:00:09 +0200

    test: adjust ViewAssemblerHelperTest to run in Spring context

--- src/test/java/de/fraunhofer/isst/dataspaceconnector/view/ViewAssemblerHelperTest.java
+++ src/test/java/de/fraunhofer/isst/dataspaceconnector/view/ViewAssemblerHelperTest.java
@@ -4,12 +4,15 @@ import java.util.UUID;
 
 import de.fraunhofer.isst.dataspaceconnector.controller.resources.ResourceControllers;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@SpringBootTest(classes = {ViewAssemblerHelper.class})
 public class ViewAssemblerHelperTest {
 
     @Test
@@ -29,6 +32,8 @@ public class ViewAssemblerHelperTest {
     @Test
     public void getSelfLink_entityIdNull_returnBasePathWithoutId() {
         /* ARRANGE */
+        final var baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .toUriString();
         final var path = ResourceControllers.ArtifactController.class
                 .getAnnotation(RequestMapping.class).value()[0];
         final var rel = "self";
@@ -39,7 +44,7 @@ public class ViewAssemblerHelperTest {
 
         /* ASSERT */
         assertNotNull(result);
-        assertEquals(path, result.getHref());
+        assertEquals(baseUrl + path, result.getHref());
         assertEquals(rel, result.getRel().value());
     }
 
@@ -47,6 +52,8 @@ public class ViewAssemblerHelperTest {
     public void getSelfLink_inputCorrect_returnSelfLink() {
         /* ARRANGE */
         final var resourceId = UUID.randomUUID();
+        final var baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .toUriString();
         final var path = ResourceControllers.ArtifactController.class
                 .getAnnotation(RequestMapping.class).value()[0];
         final var rel = "self";
@@ -57,7 +64,7 @@ public class ViewAssemblerHelperTest {
 
         /* ASSERT */
         assertNotNull(result);
-        assertEquals(path + "/" + resourceId, result.getHref());
+        assertEquals(baseUrl + path + "/" + resourceId, result.getHref());
         assertEquals(rel, result.getRel().value());
     }
 
