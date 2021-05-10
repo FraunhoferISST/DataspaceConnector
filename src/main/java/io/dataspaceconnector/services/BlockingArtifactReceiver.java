3383b2bf83122587e3c4d0e1f2d710b2f703fdc5
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-05-10 20:06:57 +0200

    chore: evaluate provider response in BlockingArtifactReceiver

--- src/main/java/io/dataspaceconnector/services/BlockingArtifactReceiver.java
+++ src/main/java/io/dataspaceconnector/services/BlockingArtifactReceiver.java
@@ -15,12 +15,15 @@
  */
 package io.dataspaceconnector.services;
 
+import io.dataspaceconnector.exceptions.PolicyRestrictionException;
 import io.dataspaceconnector.model.QueryInput;
 import io.dataspaceconnector.services.messages.types.ArtifactRequestService;
 import io.dataspaceconnector.services.resources.ArtifactService;
+import io.dataspaceconnector.utils.ErrorMessages;
 import io.dataspaceconnector.utils.MessageUtils;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Base64Utils;
 
@@ -34,6 +37,7 @@ import java.util.UUID;
  * completed.
  */
 @Component
+@Log4j2
 @RequiredArgsConstructor
 public class BlockingArtifactReceiver implements ArtifactRetriever {
 
@@ -63,10 +67,20 @@ public class BlockingArtifactReceiver implements ArtifactRetriever {
      */
     @Override
     public InputStream retrieve(final UUID artifactId, final URI recipient,
-                                final URI transferContract, final QueryInput queryInput) {
+                                final URI transferContract, final QueryInput queryInput)
+            throws PolicyRestrictionException {
         final var artifact = artifactService.get(artifactId);
         final var response = messageService.sendMessage(recipient,
                 artifact.getRemoteId(), transferContract, queryInput);
+        if (!messageService.validateResponse(response)) {
+            final var content = messageService.getResponseContent(response);
+            if (log.isDebugEnabled()) {
+                log.debug("Data could not be loaded: \n" + content);
+            }
+
+            throw new PolicyRestrictionException(ErrorMessages.POLICY_RESTRICTION);
+        }
+
         final var data = MessageUtils.extractPayloadFromMultipartMessage(response);
         return new ByteArrayInputStream(Base64Utils.decodeFromString(data));
     }
