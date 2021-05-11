97f40c47d30b80b84b798ac7184f3279144124f9
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-05-11 11:18:45 +0200

    test: add test for ArtifactResponseMessageDesc

---
+++ src/test/java/io/dataspaceconnector/model/messages/ArtifactResponseMessageDescTest.java
@@ -0,0 +1,45 @@
+package io.dataspaceconnector.model.messages;
+
+import java.net.URI;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArtifactResponseMessageDescTest {
+    @Test
+    void defaultConstructor_nothing_emptyDesc() {
+        /* ARRANGE */
+        // Nothing to arrange here.
+
+        /* ACT */
+        final var result = new ArtifactResponseMessageDesc();
+
+        /* ASSERT */
+        assertNull(result.getRecipient());
+        assertNull(result.getCorrelationMessage());
+        assertNull(result.getTransferContract());
+    }
+
+    @Test
+    void allArgsConstructor_valid_validDesc() {
+        /* ARRANGE */
+        final var recipient = URI.create("someRecipient");
+        final var message = URI.create("someMessage");
+        final var contract = URI.create("someContract");
+
+        /* ACT */
+        final var result = new ArtifactResponseMessageDesc(recipient, message, contract);
+
+        /* ASSERT */
+        assertEquals(recipient, result.getRecipient());
+        assertEquals(message, result.getCorrelationMessage());
+        assertEquals(contract, result.getTransferContract());
+    }
+
+    @Test
+    void equals_everything_valid() {
+        EqualsVerifier.simple().forClass(ArtifactResponseMessageDesc.class).verify();
+    }
+}
