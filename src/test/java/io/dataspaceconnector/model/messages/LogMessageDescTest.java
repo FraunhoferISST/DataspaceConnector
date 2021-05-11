fcde1fc4499fba18bc0039688c45e27b550ed5cd
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-05-11 11:30:38 +0200

    test: add test for LogMessageDesc

---
+++ src/test/java/io/dataspaceconnector/model/messages/LogMessageDescTest.java
@@ -0,0 +1,39 @@
+package io.dataspaceconnector.model.messages;
+
+import java.net.URI;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogMessageDescTest {
+    @Test
+    void defaultConstructor_nothing_emptyDesc() {
+        /* ARRANGE */
+        // Nothing to arrange here.
+
+        /* ACT */
+        final var result = new LogMessageDesc();
+
+        /* ASSERT */
+        assertNull(result.getRecipient());
+    }
+
+    @Test
+    void allArgsConstructor_valid_validDesc() {
+        /* ARRANGE */
+        final var recipient = URI.create("someRecipient");
+
+        /* ACT */
+        final var result = new LogMessageDesc(recipient);
+
+        /* ASSERT */
+        assertEquals(recipient, result.getRecipient());
+    }
+
+    @Test
+    void equals_everything_valid() {
+        EqualsVerifier.simple().forClass(LogMessageDesc.class).verify();
+    }
+}
