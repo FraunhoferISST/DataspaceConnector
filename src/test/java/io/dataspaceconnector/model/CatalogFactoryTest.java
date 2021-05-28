0a9de1f1026ec5457fc17c454f9d82104dcabf07
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-05-28 07:53:00 +0200

    chore: further remove unused imports

--- src/test/java/io/dataspaceconnector/model/CatalogFactoryTest.java
+++ src/test/java/io/dataspaceconnector/model/CatalogFactoryTest.java
@@ -23,10 +23,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CatalogFactoryTest {
 
