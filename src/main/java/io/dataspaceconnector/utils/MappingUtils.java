fd1c9061e95ccf1befd87c9595feea903ecc3f07
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-06-07 08:41:18 +0200

    chore: fix NPE when contract start or end not set

--- src/main/java/io/dataspaceconnector/utils/MappingUtils.java
+++ src/main/java/io/dataspaceconnector/utils/MappingUtils.java
@@ -83,9 +83,9 @@ public final class MappingUtils {
     }
 
     private static Map<String, String> buildAdditionalForResource(final Resource resource) {
-        final var additional = propertiesToAdditional(resource.getProperties());
-
         Utils.requireNonNull(resource, ErrorMessages.ENTITY_NULL);
+
+        final var additional = propertiesToAdditional(resource.getProperties());
 
         final var periodicity = resource.getAccrualPeriodicity();
         final var contentPart = resource.getContentPart();
@@ -138,9 +138,9 @@ public final class MappingUtils {
             additional.put("ids:modified", modified.toXMLFormat());
         }
 
-        // if (resourceEndpoint != null) {
-            //addListToAdditional(resourceEndpoint, additional, "ids:resourceEndpoint");
-        // }
+        if (resourceEndpoint != null) {
+            addListToAdditional(resourceEndpoint, additional, "ids:resourceEndpoint");
+        }
 
         if (resourcePart != null) {
             addListToAdditional(resourcePart, additional, "ids:resourcePart");
@@ -186,41 +186,30 @@ public final class MappingUtils {
         final var description = resource.getDescription();
         final var keywords = IdsUtils.getKeywordsAsString(resource.getKeyword());
         final var language = resource.getLanguage();
-        final var additional = buildAdditionalForResource(resource);
         final var publisher = resource.getPublisher();
         final var sovereign = resource.getSovereign();
         final var standardLicense = resource.getStandardLicense();
         final var title = resource.getTitle();
         final var resourceEndpoint = resource.getResourceEndpoint();
 
-        desc.setAdditional(additional);
+        desc.setAdditional(buildAdditionalForResource(resource));
         desc.setKeywords(keywords);
         desc.setPublisher(publisher);
         desc.setLicence(standardLicense);
         desc.setSovereign(sovereign);
 
         if (description != null) {
-            if (description.size() == 1) {
-                desc.setDescription(description.get(0).toString());
-            } else {
-                desc.setDescription(description.toString());
-            }
+            desc.setDescription(description.size() == 1 ? description.get(0).toString()
+                                                        : description.toString());
         }
 
         if (title != null) {
-            if (title.size() == 1) {
-                desc.setTitle(title.get(0).toString());
-            } else {
-                desc.setTitle(title.toString());
-            }
+            desc.setTitle(title.size() == 1 ? title.get(0).toString() : title.toString());
         }
 
         if (language != null) {
-            if (language.size() == 1) {
-                desc.setLanguage(language.get(0).toString());
-            } else {
-                desc.setLanguage(language.toString());
-            }
+            desc.setLanguage(
+                    language.size() == 1 ? language.get(0).toString() : language.toString());
         }
 
         if (resourceEndpoint != null) {
@@ -274,11 +263,7 @@ public final class MappingUtils {
     private static void addListToAdditional(final List<?> list,
                                             final Map<String, String> additional,
                                             final String key) {
-        if (list.size() == 1) {
-            additional.put(key, list.get(0).toString());
-        } else {
-            additional.put(key, list.toString());
-        }
+        additional.put(key, list.size() == 1 ? list.get(0).toString() : list.toString());
     }
 
     /**
@@ -415,16 +400,20 @@ public final class MappingUtils {
         desc.setProvider(provider);
         desc.setRemoteId(contractId);
 
-        try {
-            desc.setEnd(getDateOf(end.toXMLFormat()));
-        } catch (Exception ignored) {
-            // Default values don't need to be set here.
+        if (end != null) {
+            try {
+                desc.setEnd(getDateOf(end.toXMLFormat()));
+            } catch (DateTimeParseException ignored) {
+                // Default values don't need to be set here.
+            }
         }
 
-        try {
-            desc.setStart(getDateOf(start.toXMLFormat()));
-        } catch (DateTimeParseException | NullPointerException ignored) {
-            // Default values don't need to be set here.
+        if (start != null) {
+            try {
+                desc.setStart(getDateOf(start.toXMLFormat()));
+            } catch (DateTimeParseException ignored) {
+                // Default values don't need to be set here.
+            }
         }
 
         return new ContractTemplate(null, desc, null);
