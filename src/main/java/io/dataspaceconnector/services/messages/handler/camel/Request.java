9a15fcfe4957bab91e18cf0398a66d4bf8f5a8c2
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2021-06-10 14:03:24 +0200

    refactor: remove unused subclasses of Request

--- src/main/java/io/dataspaceconnector/services/messages/handler/camel/Request.java
+++ src/main/java/io/dataspaceconnector/services/messages/handler/camel/Request.java
@@ -1,7 +1,5 @@
 package io.dataspaceconnector.services.messages.handler.camel;
 
-import de.fraunhofer.iais.eis.Message;
-import de.fraunhofer.isst.ids.framework.messaging.model.messages.MessagePayload;
 import lombok.Data;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -11,17 +9,4 @@ import lombok.RequiredArgsConstructor;
 public class Request<H, B> implements RouteMsg<H, B> {
     private final @NonNull H header;
     private final @NonNull B body;
-}
-
-
-class IdsRequest<H extends de.fraunhofer.iais.eis.Message, B> extends Request<H , B>{
-    public IdsRequest(final H header, final B body) {
-        super(header, body);
-    }
-}
-
-class IdsRawRequest<H extends Message> extends IdsRequest<H, MessagePayload> {
-    public IdsRawRequest(final H header, final MessagePayload body) {
-        super(header, body);
-    }
 }
