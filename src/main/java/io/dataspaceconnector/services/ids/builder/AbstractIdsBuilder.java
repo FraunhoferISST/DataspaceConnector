6763b74e4d733e78af36d61b5e992943ed2b62e4
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-06-11 09:58:35 +0200

    chore: remove unused import

--- src/main/java/io/dataspaceconnector/services/ids/builder/AbstractIdsBuilder.java
+++ src/main/java/io/dataspaceconnector/services/ids/builder/AbstractIdsBuilder.java
@@ -18,7 +18,6 @@ package io.dataspaceconnector.services.ids.builder;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
