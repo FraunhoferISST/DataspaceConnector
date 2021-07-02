49578d95757116f36820fa0c7ed9dd0f85403172
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-07-02 13:49:09 +0200

    chore: add java docs

--- src/main/java/io/dataspaceconnector/bootstrap/BootstrapLoader.java
+++ src/main/java/io/dataspaceconnector/bootstrap/BootstrapLoader.java
@@ -7,12 +7,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
+/**
+ * Loads the bootstrapper on initialization.
+ */
 @ConditionalOnProperty(value = "bootstrap.enabled", havingValue = "true", matchIfMissing = true)
 @RequiredArgsConstructor
 @Service
-public class BootstrapLoader {
+public final class BootstrapLoader {
     private final @NonNull Bootstrapper bootstrapper;
 
+    /**
+     * Load the bootstrapper.
+     */
     @PostConstruct
     public void loadBootstrapper() {
         bootstrapper.bootstrap();
