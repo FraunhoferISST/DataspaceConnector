977d85923d289402a643454bb54785620fbab8bc
omar <OmarLuiz.BarretoSilva@isst.fraunhofer.de>
2021-07-13 12:26:49 +0200

    refactor: move petrinet test package

--- src/test/java/io/configmanager/extensions/routes/petrinet/InfomodelPetriNetBuilderTest.java
+++ src/test/java/io/configmanager/extensions/routes/petrinet/InfomodelPetriNetBuilderTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.configmanager.petrinet;
+package io.configmanager.extensions.routes.petrinet;
 
 import de.fraunhofer.iais.eis.AppRouteBuilder;
 import de.fraunhofer.iais.eis.Endpoint;
