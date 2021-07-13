d946e5eaa34ee06d5996e5a38ee5ab1f409ad271
Gökhan Kahriman <goekhan.kahriman@isst.fraunhofer.de>
2021-07-13 12:35:11 +0200

    test: add test for factory classes

---
+++ src/test/java/io/dataspaceconnector/model/route/RouteFactoryTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020 Fraunhofer Institute for Software and Systems Engineering
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.dataspaceconnector.model.route;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RouteFactoryTest {
+
+    final RouteDesc desc = new RouteDesc();
+    final RouteFactory factory = new RouteFactory();
+
+    @Test
+    void create_validDesc_returnNew() {
+        /* ARRANGE */
+        final var title = "MyRoute";
+        final var description = "MyDesc";
+        final var routeType = RouteType.ROUTE;
+        desc.setTitle(title);
+        desc.setDescription(description);
+        desc.setRouteType(routeType);
+
+        /* ACT */
+        final var result = factory.create(desc);
+
+        /* ASSERT */
+        assertEquals(title, result.getTitle());
+        assertEquals(description, result.getDescription());
+        assertEquals(routeType, result.getRouteType());
+    }
+}
