9ae634da1f3b4c3c78b0dec5243009d9162ada43
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-07-15 07:57:33 +0200

    refactor: move view.route into own package

--- src/main/java/io/dataspaceconnector/view/route/RouteView.java
+++ src/main/java/io/dataspaceconnector/view/route/RouteView.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.dataspaceconnector.view;
+package io.dataspaceconnector.view.route;
 
 import java.time.ZonedDateTime;
 import java.util.Map;
--- src/main/java/io/dataspaceconnector/view/route/RouteViewAssembler.java
+++ src/main/java/io/dataspaceconnector/view/route/RouteViewAssembler.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.dataspaceconnector.view;
+package io.dataspaceconnector.view.route;
 
 import io.dataspaceconnector.controller.configurations.RouteControllers;
 import io.dataspaceconnector.controller.resource.view.SelfLinking;
