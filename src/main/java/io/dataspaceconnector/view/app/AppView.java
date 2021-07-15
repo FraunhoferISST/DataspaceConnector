9a9e7468a9483e8d060999a494e3bc5fbf67356a
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-07-15 07:53:27 +0200

    refactor: move view.app into own package

--- src/main/java/io/dataspaceconnector/view/app/AppView.java
+++ src/main/java/io/dataspaceconnector/view/app/AppView.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.dataspaceconnector.view;
+package io.dataspaceconnector.view.app;
 
 import java.time.ZonedDateTime;
 
--- src/main/java/io/dataspaceconnector/view/app/AppViewAssembler.java
+++ src/main/java/io/dataspaceconnector/view/app/AppViewAssembler.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.dataspaceconnector.view;
+package io.dataspaceconnector.view.app;
 
 import io.dataspaceconnector.controller.configurations.AppControllers.AppController;
 import io.dataspaceconnector.controller.resource.view.SelfLinking;
