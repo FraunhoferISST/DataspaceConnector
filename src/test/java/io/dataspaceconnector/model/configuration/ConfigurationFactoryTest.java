c2ea878ef7dec3b34bcfe2b9f7bce9e1e6bc413a
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-07-16 13:33:02 +0200

    test: add Configuration tests

--- src/test/java/io/dataspaceconnector/model/configuration/ConfigurationFactoryTest.java
+++ src/test/java/io/dataspaceconnector/model/configuration/ConfigurationFactoryTest.java
@@ -18,12 +18,17 @@ package io.dataspaceconnector.model.configuration;
 import java.net.URI;
 import java.util.List;
 
+import io.dataspaceconnector.model.keystore.KeystoreDesc;
 import io.dataspaceconnector.model.keystore.KeystoreFactory;
+import io.dataspaceconnector.model.proxy.ProxyDesc;
 import io.dataspaceconnector.model.proxy.ProxyFactory;
+import io.dataspaceconnector.model.truststore.TruststoreDesc;
 import io.dataspaceconnector.model.truststore.TruststoreFactory;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ConfigurationFactoryTest {
@@ -38,7 +43,7 @@ public class ConfigurationFactoryTest {
             new ConfigurationFactory(proxyFactory,truststoreFactory, keystoreFactory);
 
     @Test
-    void create_validDesc_returnNew() {
+    void create_emptyDesc_returnNew() {
         /* ARRANGE */
 
         /* ACT */
@@ -55,6 +60,33 @@ public class ConfigurationFactoryTest {
     }
 
     @Test
+    void create_validDesc_returnNew() {
+        /* ARRANGE */
+        final var desc = new ConfigurationDesc();
+        desc.setConnectorEndpoint(URI.create("https://endpoint"));
+        desc.setSecurityProfile(SecurityProfile.TRUST_PLUS_SECURITY);
+        desc.setOutboundModelVersion("fred");
+        desc.setInboundModelVersion(List.of("Model", "Version"));
+        desc.setMaintainer(URI.create("https://someMaintainer"));
+        desc.setCurator(URI.create("https://someMaintainer"));
+        desc.setVersion("fred");
+        desc.setLogLevel(LogLevel.ERROR);
+        desc.setDeployMode(DeployMode.PRODUCTIVE);
+
+        /* ACT */
+        final var result = factory.create(desc);
+
+        /* ASSERT */
+        assertEquals(desc.getConnectorEndpoint(), result.getConnectorEndpoint());
+        assertEquals(desc.getDeployMode(), result.getDeployMode());
+        assertEquals(desc.getVersion(), result.getVersion());
+        assertEquals(desc.getCurator(), result.getCurator());
+        assertEquals(desc.getOutboundModelVersion(), result.getOutboundModelVersion());
+        assertEquals(desc.getSecurityProfile(), result.getSecurityProfile());
+        assertEquals(desc.getInboundModelVersion(), result.getInboundModelVersion());
+    }
+
+    @Test
     void update_newConnectorEndpoint_willUpdate() {
         /* ARRANGE */
         final var config = factory.create(new ConfigurationDesc());
@@ -62,9 +94,10 @@ public class ConfigurationFactoryTest {
         desc.setConnectorEndpoint(URI.create("https://endpoint"));
 
         /* ACT */
-        factory.update(config, desc);
+        final var result = factory.update(config, desc);
 
         /* ASSERT */
+        assertTrue(result);
         assertEquals(desc.getConnectorEndpoint(), config.getConnectorEndpoint());
     }
 
@@ -76,9 +109,10 @@ public class ConfigurationFactoryTest {
         desc.setSecurityProfile(SecurityProfile.TRUST_PLUS_SECURITY);
 
         /* ACT */
-        factory.update(config, desc);
+        final var result = factory.update(config, desc);
 
         /* ASSERT */
+        assertTrue(result);
         assertEquals(desc.getSecurityProfile(), config.getSecurityProfile());
     }
 
@@ -90,9 +124,10 @@ public class ConfigurationFactoryTest {
         desc.setOutboundModelVersion("fred");
 
         /* ACT */
-        factory.update(config, desc);
+        final var result = factory.update(config, desc);
 
         /* ASSERT */
+        assertTrue(result);
         assertEquals(desc.getOutboundModelVersion(), config.getOutboundModelVersion());
     }
 
@@ -104,9 +139,10 @@ public class ConfigurationFactoryTest {
         desc.setInboundModelVersion(List.of("Model", "Version"));
 
         /* ACT */
-        factory.update(config, desc);
+        final var result = factory.update(config, desc);
 
         /* ASSERT */
+        assertTrue(result);
         assertEquals(desc.getInboundModelVersion(), config.getInboundModelVersion());
     }
 
@@ -118,9 +154,10 @@ public class ConfigurationFactoryTest {
         desc.setMaintainer(URI.create("https://someMaintainer"));
 
         /* ACT */
-        factory.update(config, desc);
+        final var result = factory.update(config, desc);
 
         /* ASSERT */
+        assertTrue(result);
         assertEquals(desc.getMaintainer(), config.getMaintainer());
     }
 
@@ -132,9 +169,10 @@ public class ConfigurationFactoryTest {
         desc.setCurator(URI.create("https://someMaintainer"));
 
         /* ACT */
-        factory.update(config, desc);
+        final var result = factory.update(config, desc);
 
         /* ASSERT */
+        assertTrue(result);
         assertEquals(desc.getCurator(), config.getCurator());
     }
 
@@ -146,9 +184,10 @@ public class ConfigurationFactoryTest {
         desc.setVersion("fred");
 
         /* ACT */
-        factory.update(config, desc);
+        final var result = factory.update(config, desc);
 
         /* ASSERT */
+        assertTrue(result);
         assertEquals(desc.getVersion(), config.getVersion());
     }
 
@@ -160,9 +199,10 @@ public class ConfigurationFactoryTest {
         desc.setLogLevel(LogLevel.ERROR);
 
         /* ACT */
-        factory.update(config, desc);
+        final var result = factory.update(config, desc);
 
         /* ASSERT */
+        assertTrue(result);
         assertEquals(desc.getLogLevel(), config.getLogLevel());
     }
 
@@ -174,9 +214,244 @@ public class ConfigurationFactoryTest {
         desc.setDeployMode(DeployMode.PRODUCTIVE);
 
         /* ACT */
-        factory.update(config, desc);
+        final var result = factory.update(config, desc);
 
         /* ASSERT */
+        assertTrue(result);
         assertEquals(desc.getDeployMode(), config.getDeployMode());
+    }
+
+    @Test
+    void update_newKeystore_willUpdate() {
+        /* ARRANGE */
+        final var config = factory.create(new ConfigurationDesc());
+        final var desc = new ConfigurationDesc();
+        final var keystoreSettings = new KeystoreDesc();
+        keystoreSettings.setPassword("RANDOM");
+        desc.setKeystoreSettings(keystoreSettings);
+        final var expected = keystoreFactory.create(desc.getKeystoreSettings());
+
+        /* ACT */
+        final var result = factory.update(config, desc);
+
+        /* ASSERT */
+        assertTrue(result);
+        assertEquals(expected, config.getKeystore());
+    }
+
+    @Test
+    void update_newTruststore_willUpdate() {
+        /* ARRANGE */
+        final var config = factory.create(new ConfigurationDesc());
+        final var desc = new ConfigurationDesc();
+        final var truststoreSettings = new TruststoreDesc();
+        truststoreSettings.setPassword("RANDOM");
+        desc.setTruststoreSettings(truststoreSettings);
+        final var expected = truststoreFactory.create(desc.getTruststoreSettings());
+
+        /* ACT */
+        final var result = factory.update(config, desc);
+
+        /* ASSERT */
+        assertTrue(result);
+        assertEquals(expected, config.getTruststore());
+    }
+
+    @Test
+    void update_sameConnectorEndpoint_willNotUpdate() {
+        /* ARRANGE */
+        final var desc = new ConfigurationDesc();
+        desc.setConnectorEndpoint(URI.create("https://endpoint"));
+        final var config = factory.create(desc);
+
+        /* ACT */
+        final var result = factory.update(config, desc);
+
+        /* ASSERT */
+        assertFalse(result);
+        assertEquals(desc.getConnectorEndpoint(), config.getConnectorEndpoint());
+    }
+
+    @Test
+    void update_sameSecurityProfile_willNotUpdate() {
+        /* ARRANGE */
+        final var desc = new ConfigurationDesc();
+        desc.setSecurityProfile(SecurityProfile.TRUST_PLUS_SECURITY);
+        final var config = factory.create(desc);
+
+        /* ACT */
+        final var result = factory.update(config, desc);
+
+        /* ASSERT */
+        assertFalse(result);
+        assertEquals(desc.getSecurityProfile(), config.getSecurityProfile());
+    }
+
+    @Test
+    void update_sameOutBoundModelVersion_willNotUpdate() {
+        /* ARRANGE */
+        final var desc = new ConfigurationDesc();
+        desc.setOutboundModelVersion("fred");
+        final var config = factory.create(desc);
+
+        /* ACT */
+        final var result = factory.update(config, desc);
+
+        /* ASSERT */
+        assertFalse(result);
+        assertEquals(desc.getOutboundModelVersion(), config.getOutboundModelVersion());
+    }
+
+    @Test
+    void update_sameInBoundModelVersion_willNotUpdate() {
+        /* ARRANGE */
+        final var desc = new ConfigurationDesc();
+        desc.setInboundModelVersion(List.of("Model", "Version"));
+        final var config = factory.create(desc);
+
+        /* ACT */
+        final var result = factory.update(config, desc);
+
+        /* ASSERT */
+        assertFalse(result);
+        assertEquals(desc.getInboundModelVersion(), config.getInboundModelVersion());
+    }
+
+    @Test
+    void update_sameMaintainer_willNotUpdate() {
+        /* ARRANGE */
+        final var desc = new ConfigurationDesc();
+        desc.setMaintainer(URI.create("https://someMaintainer"));
+        final var config = factory.create(desc);
+
+        /* ACT */
+        final var result = factory.update(config, desc);
+
+        /* ASSERT */
+        assertFalse(result);
+        assertEquals(desc.getMaintainer(), config.getMaintainer());
+    }
+
+    @Test
+    void update_sameCurator_willNotUpdate() {
+        /* ARRANGE */
+        final var desc = new ConfigurationDesc();
+        desc.setCurator(URI.create("https://someMaintainer"));
+        final var config = factory.create(desc);
+
+        /* ACT */
+        final var result = factory.update(config, desc);
+
+        /* ASSERT */
+        assertFalse(result);
+        assertEquals(desc.getCurator(), config.getCurator());
+    }
+
+    @Test
+    void update_sameVersion_willNotUpdate() {
+        /* ARRANGE */
+        final var desc = new ConfigurationDesc();
+        desc.setVersion("fred");
+        final var config = factory.create(desc);
+
+        /* ACT */
+        final var result = factory.update(config, desc);
+
+        /* ASSERT */
+        assertFalse(result);
+        assertEquals(desc.getVersion(), config.getVersion());
+    }
+
+    @Test
+    void update_sameLogLevel_willNotUpdate() {
+        /* ARRANGE */
+        final var desc = new ConfigurationDesc();
+        desc.setLogLevel(LogLevel.ERROR);
+        final var config = factory.create(desc);
+
+        /* ACT */
+        final var result = factory.update(config, desc);
+
+        /* ASSERT */
+        assertFalse(result);
+        assertEquals(desc.getLogLevel(), config.getLogLevel());
+    }
+
+    @Test
+    void update_sameDeployMode_willNotUpdate() {
+        /* ARRANGE */
+        final var desc = new ConfigurationDesc();
+        desc.setDeployMode(DeployMode.PRODUCTIVE);
+        final var config = factory.create(desc);
+
+        /* ACT */
+        final var result = factory.update(config, desc);
+
+        /* ASSERT */
+        assertFalse(result);
+        assertEquals(desc.getDeployMode(), config.getDeployMode());
+    }
+
+    @Test
+    void update_sameKeystore_willNotUpdate() {
+        /* ARRANGE */
+        final var desc = new ConfigurationDesc();
+        desc.setKeystoreSettings(new KeystoreDesc());
+        final var config = factory.create(desc);
+        final var expected = keystoreFactory.create(desc.getKeystoreSettings());
+
+        /* ACT */
+        final var result = factory.update(config, desc);
+
+        /* ASSERT */
+        assertFalse(result);
+        assertEquals(expected, config.getKeystore());
+    }
+
+    @Test
+    void update_sameTruststore_willNotUpdate() {
+        /* ARRANGE */
+        final var desc = new ConfigurationDesc();
+        desc.setTruststoreSettings(new TruststoreDesc());
+        final var config = factory.create(desc);
+        final var expected = truststoreFactory.create(desc.getTruststoreSettings());
+
+        /* ACT */
+        final var result = factory.update(config, desc);
+
+        /* ASSERT */
+        assertFalse(result);
+        assertEquals(expected, config.getTruststore());
+    }
+
+    @Test
+    void update_newNullProxy_willUpdate() {
+        /* ARRANGE */
+        final var desc = new ConfigurationDesc();
+        desc.setProxySettings(new ProxyDesc());
+        final var config = factory.create(desc);
+
+        /* ACT */
+        final var result = factory.update(config, new ConfigurationDesc());
+
+        /* ASSERT */
+        assertTrue(result);
+        assertNull(config.getProxy());
+    }
+
+    @Test
+    void update_newProxy_willUpdate() {
+        /* ARRANGE */
+        final var desc = new ConfigurationDesc();
+        desc.setProxySettings(new ProxyDesc());
+        final var config = factory.create(new ConfigurationDesc());
+        final var expected = proxyFactory.create(desc.getProxySettings());
+
+        /* ACT */
+        final var result = factory.update(config, desc);
+
+        /* ASSERT */
+        assertTrue(result);
+        assertEquals(expected, config.getProxy());
     }
 }
