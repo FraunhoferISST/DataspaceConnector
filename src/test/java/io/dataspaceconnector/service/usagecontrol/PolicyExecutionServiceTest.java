8ece7f5ebc36976e42046c85e4eeb35d5736113e
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-07-17 11:01:19 +0200

    test: update PolicyExcutionServiceTests

--- src/test/java/io/dataspaceconnector/service/usagecontrol/PolicyExecutionServiceTest.java
+++ src/test/java/io/dataspaceconnector/service/usagecontrol/PolicyExecutionServiceTest.java
@@ -15,6 +15,9 @@
  */
 package io.dataspaceconnector.service.usagecontrol;
 
+import java.net.URI;
+import java.util.UUID;
+
 import de.fraunhofer.iais.eis.Action;
 import de.fraunhofer.iais.eis.BinaryOperator;
 import de.fraunhofer.iais.eis.ConstraintBuilder;
@@ -39,9 +42,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.net.URI;
-import java.util.UUID;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
@@ -50,7 +50,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = {PolicyExecutionService.class})
+@SpringBootTest(classes = {PolicyExecutionService.class, ClearingHouseService.class, LogBuilder.class})
 public class PolicyExecutionServiceTest {
 
     @MockBean
