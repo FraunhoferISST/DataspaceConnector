70fdc7480b6b780ff02c9770c073cb1571e15242
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2021-07-17 15:41:58 +0200

    chore: add RouteManager to ConnectorEndpointService

--- src/main/java/io/dataspaceconnector/service/configuration/ConnectorEndpointService.java
+++ src/main/java/io/dataspaceconnector/service/configuration/ConnectorEndpointService.java
@@ -15,16 +15,70 @@
  */
 package io.dataspaceconnector.service.configuration;
 
+import io.configmanager.extensions.routes.camel.RouteManager;
 import io.dataspaceconnector.model.endpoint.ConnectorEndpoint;
 import io.dataspaceconnector.model.endpoint.ConnectorEndpointDesc;
-import lombok.NoArgsConstructor;
+import io.dataspaceconnector.repository.RouteRepository;
+import io.dataspaceconnector.service.ids.builder.IdsAppRouteBuilder;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
  * Service class for connector endpoints.
  */
 @Service
-@NoArgsConstructor
 public class ConnectorEndpointService
         extends EndpointService<ConnectorEndpoint, ConnectorEndpointDesc> {
+
+    /**
+     * Service for managing routes.
+     */
+    private final @NonNull RouteRepository routeRepo;
+
+    /**
+     * Service for managing Camel routes.
+     */
+    private final @NonNull RouteManager routeManager;
+
+    /**
+     * Service for creating IDS AppRoutes from routes.
+     */
+    private final @NonNull IdsAppRouteBuilder appRouteBuilder;
+
+    /**
+     * Constructor for injection.
+     *
+     * @param routeRepository the service for managing routes.
+     * @param camelRouteManager the Camel route manager.
+     * @param idsAppRouteBuilder the AppRoute builder.
+     */
+    @Autowired
+    public ConnectorEndpointService(final @NonNull RouteRepository routeRepository,
+                                    final @NonNull RouteManager camelRouteManager,
+                                    final @NonNull IdsAppRouteBuilder idsAppRouteBuilder) {
+        super();
+        this.routeRepo = routeRepository;
+        this.routeManager = camelRouteManager;
+        this.appRouteBuilder = idsAppRouteBuilder;
+    }
+
+    /**
+     * Persists a connector endpoint. If an already existing endpoint is updated, the Camel routes
+     * for all routes referencing the endpoint are recreated.
+     *
+     * @param endpoint the endpoint to persist.
+     * @return the persisted endpoint.
+     */
+    @Override
+    protected final ConnectorEndpoint persist(final ConnectorEndpoint endpoint) {
+        if (endpoint.getId() != null) {
+            final var affectedRoutes = routeRepo.findTopLevelRoutesByEndpoint(endpoint.getId());
+            affectedRoutes.forEach(r -> routeManager
+                    .createAndDeployXMLRoute(appRouteBuilder.create(r)));
+        }
+
+        return super.persist(endpoint);
+    }
+
 }
