e3e7a77d5fb3c20b967ebcc87b01bb7f0f6c596b
Brian-Frederik Jahnke <brian-frederik.jahnke@isst.fraunhofer.de>
2021-07-20 19:18:28 +0200

    chore: change shell script init lines

--- scripts/ci/e2e/setup-provider-consumer.sh
+++ scripts/ci/e2e/setup-provider-consumer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Copyright 2020 Fraunhofer Institute for Software and Systems Engineering
 #
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+set -eo pipefail
 
 echo "Setup provider and consumer"
 
