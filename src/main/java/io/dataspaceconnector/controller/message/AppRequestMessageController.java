87e43b8e30c7f633247d373aaeb670c1f743bc5c
eakker <erik.van.den.akker@isst.fraunhofer.de>
2021-07-28 08:22:47 +0200

    chore: add sending of DescriptionRequestMessage to AppRequestMessageController

--- src/main/java/io/dataspaceconnector/controller/message/AppRequestMessageController.java
+++ src/main/java/io/dataspaceconnector/controller/message/AppRequestMessageController.java
@@ -1,9 +1,19 @@
 package io.dataspaceconnector.controller.message;
 
+import io.dataspaceconnector.camel.dto.Response;
+import io.dataspaceconnector.camel.util.ParameterUtils;
+import io.dataspaceconnector.config.ConnectorConfiguration;
+import io.dataspaceconnector.controller.util.ControllerUtils;
 import io.dataspaceconnector.exception.MessageException;
 import io.dataspaceconnector.exception.MessageResponseException;
 import io.dataspaceconnector.exception.UnexpectedResponseException;
 import io.dataspaceconnector.service.MetadataDownloader;
+import io.dataspaceconnector.service.ids.ConnectorService;
+import io.dataspaceconnector.service.ids.DeserializationService;
+import io.dataspaceconnector.service.message.GlobalMessageService;
+import io.dataspaceconnector.service.message.type.DescriptionRequestService;
+import io.dataspaceconnector.util.MessageUtils;
+import io.dataspaceconnector.util.Utils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -11,6 +21,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.ExchangeBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +37,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.persistence.PersistenceException;
 import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 /**
@@ -37,9 +51,30 @@ import java.util.UUID;
 public class AppRequestMessageController {
 
     /**
-     * Downloads metadata.
+     * Service for message handling.
      */
-    private final @NonNull MetadataDownloader metadataDownloader;
+    private final @NonNull DescriptionRequestService descriptionReqSvc;
+
+    /**
+     * Service for ids deserialization.
+     */
+    private final @NonNull DeserializationService deserializationSvc;
+
+    /**
+     * Template for triggering Camel routes.
+     */
+    private final @NonNull ProducerTemplate template;
+
+    /**
+     * The CamelContext required for constructing the {@link ProducerTemplate}.
+     */
+    private final @NonNull CamelContext context;
+
+    /**
+     * Service for handle application.properties settings.
+     */
+    private final @NonNull ConnectorConfiguration connectorConfig;
+
 
     @PostMapping("/app")
     @Operation(summary = "Send IDS App request message")
@@ -52,12 +87,68 @@ public class AppRequestMessageController {
             @RequestParam("recipient") final URI recipient,
             @Parameter(description = "The app url.", required = true)
             @RequestParam("app") final URI app) {
+        //Just send a descriptionRequestmessage to get the app information
+        //TODO: convert response to App object
+        String payload;
+        if (connectorConfig.isIdscpEnabled()) {
+            final var result = template.send("direct:descriptionRequestSender",
+                    ExchangeBuilder.anExchange(context)
+                            .withProperty(ParameterUtils.RECIPIENT_PARAM, recipient)
+                            .withProperty(ParameterUtils.ELEMENT_ID_PARAM, app)
+                            .build());
 
-        downloadApp(recipient, app);
-        return new ResponseEntity<Object>(HttpStatus.OK);
+            final var response = result.getIn().getBody(Response.class);
+            if (response != null) {
+                payload = response.getBody();
+            } else {
+                final var responseEntity =
+                        toObjectResponse(result.getIn().getBody(ResponseEntity.class));
+                return Objects.requireNonNullElseGet(responseEntity,
+                        () -> new ResponseEntity<>("An internal server error occurred.",
+                                HttpStatus.INTERNAL_SERVER_ERROR));
+            }
+        } else {
+            try {
+                // Send and validate description request/response message.
+                final var response = descriptionReqSvc.sendMessage(recipient, app);
+
+                // Read and process the response message.
+                payload = MessageUtils.extractPayloadFromMultipartMessage(response);
+            } catch (MessageException exception) {
+                // If the message could not be built.
+                return ControllerUtils.respondIdsMessageFailed(exception);
+            } catch (MessageResponseException | IllegalArgumentException e) {
+                // If the response message is invalid or malformed.
+                return ControllerUtils.respondReceivedInvalidResponse(e);
+            } catch (UnexpectedResponseException e) {
+                // If the response is not as expected.
+                return ControllerUtils.respondWithContent(e.getContent());
+            }
+        }
+        return new ResponseEntity<>(convertToAnswer(app, payload), HttpStatus.OK);
+    }
+
+    private String convertToAnswer(final URI elementId, final String payload) {
+        return Utils.isEmptyOrNull(elementId) ? unwrapResponse(payload) : payload;
+    }
+
+    private String unwrapResponse(final String payload) {
+        try {
+            // Get payload as component.
+            return deserializationSvc.getInfrastructureComponent(payload).toRdf();
+        } catch (IllegalArgumentException ignored) {
+            // If the response is not of type base connector.
+            return payload;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static ResponseEntity<Object> toObjectResponse(final ResponseEntity<?> response) {
+        return (ResponseEntity<Object>) response;
     }
 
     private void downloadApp(final URI recipient, final URI appResource) {
+          //TODO apps should be downloaded when they are started, not after the request
 //        metadataDownloader.downloadAppResource(recipient, appResource);
     }
 }
