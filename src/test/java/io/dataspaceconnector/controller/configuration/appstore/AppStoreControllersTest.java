a60aa0cfeee5dff868b61a9e5b8ed8b7056f70cd
Tim Berthold <tim.berthold@isst.fraunhofer.de>
2021-07-30 16:37:59 +0200

    test: add AppStoreControllersTest

---
+++ src/test/java/io/dataspaceconnector/controller/configuration/appstore/AppStoreControllersTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2020 Fraunhofer Institute for Software and Systems Engineering
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.dataspaceconnector.controller.configuration.appstore;
+
+import io.dataspaceconnector.controller.resource.exception.MethodNotAllowed;
+import io.dataspaceconnector.service.appstore.AppStoreRegistryService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class AppStoreControllersTest {
+
+    @MockBean
+    private AppStoreRegistryService appStoreRegistryService;
+
+    @Autowired
+    private AppStoreControllers.AppController appController;
+
+    @Autowired
+    private AppStoreControllers.AppStoreRegistryController appStoreRegistryController;
+
+    @Test
+    public void createApp_returnMethodNotAllowed() {
+        /* ARRANGE */
+        // Nothing to arrange.
+
+        /* ACT && ASSERT */
+        assertThrows(MethodNotAllowed.class, () -> appController.create(null));
+    }
+
+    @Test
+    public void updateApp_returnMethodNotAllowed() {
+        /* ARRANGE */
+        // Nothing to arrange.
+
+        /* ACT && ASSERT */
+        assertThrows(MethodNotAllowed.class, () -> appController.update(null, null));
+    }
+
+    @Test
+    public void getImages() {
+        /* ARRANGE */
+        // Nothing to arrange.
+
+        /* ACT && ASSERT */
+        assertEquals(HttpStatus.OK, appStoreRegistryController.getImages().getStatusCode());
+    }
+
+    @Test
+    public void getContainers() {
+        /* ARRANGE */
+        // Nothing to arrange.
+
+        /* ACT && ASSERT */
+        assertEquals(HttpStatus.OK, appStoreRegistryController.getContainers().getStatusCode());
+    }
+}
