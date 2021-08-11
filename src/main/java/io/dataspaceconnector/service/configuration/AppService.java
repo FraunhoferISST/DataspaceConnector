b349b03504c955c21616f1384ed4d8f991a4b040
Gökhan Kahriman <goekhan.kahriman@isst.fraunhofer.de>
2021-08-11 11:53:36 +0200

    chore: app template is persisted in a JSON file

--- src/main/java/io/dataspaceconnector/service/configuration/AppService.java
+++ src/main/java/io/dataspaceconnector/service/configuration/AppService.java
@@ -19,21 +19,29 @@ import io.dataspaceconnector.model.app.App;
 import io.dataspaceconnector.model.app.AppDesc;
 import io.dataspaceconnector.model.app.AppImpl;
 import io.dataspaceconnector.model.appstore.AppStore;
+import io.dataspaceconnector.model.artifact.Data;
+import io.dataspaceconnector.model.artifact.DataType;
 import io.dataspaceconnector.model.artifact.LocalData;
 import io.dataspaceconnector.repository.DataRepository;
 import io.dataspaceconnector.service.resource.BaseEntityService;
 import io.dataspaceconnector.util.exception.NotImplemented;
 import lombok.NonNull;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * Service class for apps.
@@ -55,7 +63,7 @@ public class AppService extends BaseEntityService<App, AppDesc> {
     /**
      * Constructor for the app service.
      * @param appStoreService The app store service.
-     * @param dataRepo The data repository.
+     * @param dataRepo        The data repository.
      */
     @Autowired
     public AppService(final @NonNull AppStoreService appStoreService,
@@ -67,7 +75,6 @@ public class AppService extends BaseEntityService<App, AppDesc> {
 
     /**
      * Get AppStores which are offering the given App.
-     *
      * @param appId    id of the app to find related appstore for.
      * @param pageable pageable for response as view.
      * @return Page containing AppStores which are offering an app with AppID.
@@ -78,35 +85,54 @@ public class AppService extends BaseEntityService<App, AppDesc> {
 
     /**
      * Update an artifacts underlying data.
-     *
      * @param appArtifactId The artifact which should be updated.
-     * @param data       The new data.
-     * @return The data stored in the artifact.
+     * @param data          The new data.
      * @throws IOException if the data could not be stored.
      */
     @NonNull
-    public InputStream setData(final UUID appArtifactId, final InputStream data)
+    public void setData(final UUID appArtifactId, final InputStream data)
             throws IOException {
         final var appArtifact = get(appArtifactId);
         final var currentData = ((AppImpl) appArtifact).getData();
         if (currentData instanceof LocalData) {
-            return setLocalData(appArtifactId, data, (LocalData) currentData);
+            setAppTemplate(appArtifactId, data, (LocalData) currentData);
         } else {
             throw new NotImplemented();
         }
     }
 
     @NonNull
-    private ByteArrayInputStream setLocalData(final UUID appArtifactId,
-                                              final InputStream data,
-                                              final LocalData localData)
+    private void setAppTemplate(final UUID appArtifactId, final InputStream data,
+                                final LocalData localData)
             throws IOException {
         try {
             // Update the internal database and return the new data.
             final var bytes = data.readAllBytes();
             data.close();
-            dataRepository.setLocalData(localData.getId(), bytes);
-            return new ByteArrayInputStream(bytes);
+            dataRepository.setAppTemplate(localData.getId(), DataType.APP_TEMPLATE, bytes);
+
+            // Create JSON file for app template.
+            final var templates = dataRepository.findAll();
+            final var appTemplates = new ArrayList<Data>();
+            var jsonTemplate = "";
+
+            for (var dataTmp : templates) {
+                if (DataType.APP_TEMPLATE.equals(dataTmp.getType())) {
+                    appTemplates.add(dataTmp);
+                }
+            }
+
+            jsonTemplate = appTemplates.stream()
+                    .map(Object::toString)
+                    .collect(Collectors.joining(",", "", ""));
+
+            final var path = Path.of("src/main/resources/Template.json");
+            final var  content = Files.readString(path, StandardCharsets.UTF_8);
+
+            final var resultTemplate = String.format(content, jsonTemplate);
+            FileUtils.writeStringToFile(new File("src/main/resources/AppTemplate.json"),
+                    resultTemplate, StandardCharsets.UTF_8);
+
         } catch (IOException e) {
             if (log.isErrorEnabled()) {
                 log.error("Failed to store data. [artifactId=({}), exception=({})]",
