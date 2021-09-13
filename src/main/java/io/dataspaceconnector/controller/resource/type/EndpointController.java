6360e9ebd4cab671496ef9485c96a999d2b2fee9
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2021-09-13 11:29:26 +0200

    chore: remove option to manually create, update or delete connector endpoints

--- src/main/java/io/dataspaceconnector/controller/resource/type/EndpointController.java
+++ src/main/java/io/dataspaceconnector/controller/resource/type/EndpointController.java
@@ -27,6 +27,8 @@ import io.dataspaceconnector.controller.util.ResponseCode;
 import io.dataspaceconnector.controller.util.ResponseDescription;
 import io.dataspaceconnector.model.endpoint.AppEndpoint;
 import io.dataspaceconnector.model.endpoint.AppEndpointDesc;
+import io.dataspaceconnector.model.endpoint.ConnectorEndpoint;
+import io.dataspaceconnector.model.endpoint.ConnectorEndpointDesc;
 import io.dataspaceconnector.model.endpoint.Endpoint;
 import io.dataspaceconnector.model.endpoint.EndpointDesc;
 import io.dataspaceconnector.service.resource.type.EndpointServiceProxy;
@@ -104,7 +106,7 @@ public class EndpointController implements CRUDController<Endpoint, EndpointDesc
      */
     @Override
     public ResponseEntity<Object> create(final EndpointDesc desc) {
-        if (isAppEndpoint(desc)) {
+        if (isAppEndpoint(desc) || isConnectorEndpoint(desc)) {
             throw new MethodNotAllowed();
         }
         return respondCreated(service.create(desc));
@@ -141,7 +143,7 @@ public class EndpointController implements CRUDController<Endpoint, EndpointDesc
      */
     @Override
     public ResponseEntity<Object> update(final UUID resourceId, final EndpointDesc desc) {
-        if (isAppEndpoint(desc)) {
+        if (isAppEndpoint(desc) || isConnectorEndpoint(desc)) {
             throw new MethodNotAllowed();
         }
         final var resource = service.update(resourceId, desc);
@@ -159,7 +161,8 @@ public class EndpointController implements CRUDController<Endpoint, EndpointDesc
      */
     @Override
     public ResponseEntity<Void> delete(final UUID resourceId) {
-        if (service.get(resourceId) instanceof AppEndpoint) {
+        if (service.get(resourceId) instanceof AppEndpoint
+                || service.get(resourceId) instanceof ConnectorEndpoint) {
             throw new MethodNotAllowed();
         }
         service.delete(resourceId);
@@ -189,5 +192,9 @@ public class EndpointController implements CRUDController<Endpoint, EndpointDesc
 
     private boolean isAppEndpoint(final EndpointDesc desc) {
         return desc instanceof AppEndpointDesc;
+    }
+
+    private boolean isConnectorEndpoint(final EndpointDesc desc) {
+        return desc instanceof ConnectorEndpointDesc;
     }
 }
