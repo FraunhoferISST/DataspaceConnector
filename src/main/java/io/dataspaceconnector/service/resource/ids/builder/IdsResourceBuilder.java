f93122534c9191a8e09277b9710f70953d7178a2
Tim Berthold <tim.berthold@isst.fraunhofer.de>
2021-09-30 09:21:32 +0200

    chore: fix checkstyle

--- src/main/java/io/dataspaceconnector/service/resource/ids/builder/IdsResourceBuilder.java
+++ src/main/java/io/dataspaceconnector/service/resource/ids/builder/IdsResourceBuilder.java
@@ -33,7 +33,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
