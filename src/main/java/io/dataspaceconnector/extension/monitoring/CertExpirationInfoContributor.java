49c92f527a2b28f54c11ad43bf3dcefae595618c
vdakker <75307507+vdakker@users.noreply.github.com>
2021-10-01 08:47:51 +0200

    chore: move CertExpirationInfoContributor to monitoring package

--- src/main/java/io/dataspaceconnector/extension/monitoring/CertExpirationInfoContributor.java
+++ src/main/java/io/dataspaceconnector/extension/monitoring/CertExpirationInfoContributor.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.dataspaceconnector.config;
+package io.dataspaceconnector.extension.monitoring;
 
 import de.fraunhofer.ids.messaging.core.config.ConfigContainer;
 import lombok.RequiredArgsConstructor;
