48d6d6ceb213df3e2065003b91e0d35254ffd26c
Pampus, Julia <julia.pampus@isst.fraunhofer.de>
2021-10-21 09:49:19 +0200

    chore: replace rule in subscription_creation.py

--- scripts/tests/subscription_creation.py
+++ scripts/tests/subscription_creation.py
@@ -59,43 +59,7 @@ offers = provider.create_offered_resource()
 representation = provider.create_representation()
 artifact = provider.create_artifact(data={"value": dataValue})
 contract = provider.create_contract()
-use_rule = provider.create_rule(
-    data={
-        "value": """{
-  "@context" : {
-    "ids" : "https://w3id.org/idsa/core/",
-    "idsc" : "https://w3id.org/idsa/code/"
-  },
-  "@type" : "ids:Permission",
-  "@id" : "https://w3id.org/idsa/autogen/permission/d504b82f-79dd-4c93-969d-937ab6a1d676",
-  "ids:description" : [ {
-    "@value" : "connector-restriction",
-    "@type" : "http://www.w3.org/2001/XMLSchema#string"
-  } ],
-  "ids:title" : [ {
-    "@value" : "Example Usage Policy",
-    "@type" : "http://www.w3.org/2001/XMLSchema#string"
-  } ],
-  "ids:action" : [ {
-    "@id" : "idsc:USE"
-  } ],
-  "ids:constraint" : [ {
-    "@type" : "ids:Constraint",
-    "@id" : "https://w3id.org/idsa/autogen/constraint/572c96ec-dd86-4b20-a849-a0ce8c255eee",
-    "ids:rightOperand" : {
-      "@value" : "https://w3id.org/idsa/autogen/baseConnector/7b934432-a85e-41c5-9f65-669219dde4ea",
-      "@type" : "http://www.w3.org/2001/XMLSchema#anyURI"
-    },
-    "ids:leftOperand" : {
-      "@id" : "idsc:SYSTEM"
-    },
-    "ids:operator" : {
-      "@id" : "idsc:SAME_AS"
-    }
-  } ]
-}"""
-    }
-)
+use_rule = provider.create_rule()
 
 ## Link resources
 provider.add_resource_to_catalog(catalog, offers)
