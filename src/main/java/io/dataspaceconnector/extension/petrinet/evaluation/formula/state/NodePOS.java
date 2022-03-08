73da5108153ec37acb1bdaff5fd2282c16547ea1
Julia Pampus <72392527+juliapampus@users.noreply.github.com>
2022-03-08 11:27:38 +0100

    chore: prepare release v7.0.3 (#940)
    * license: update year in license header
    
    * chore: increase version from v7.0.2 to v7.0.3
    
    * fix: revert changes to contract_negotiation_notify_access.py
    
    * docs: update README.md

--- src/main/java/io/dataspaceconnector/extension/petrinet/evaluation/formula/state/NodePOS.java
+++ src/main/java/io/dataspaceconnector/extension/petrinet/evaluation/formula/state/NodePOS.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Fraunhofer Institute for Software and Systems Engineering
+ * Copyright 2020-2022 Fraunhofer Institute for Software and Systems Engineering
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/main/java/io/dataspaceconnector/extension/telemetry/config/package-info.java
+++ src/main/java/io/dataspaceconnector/extension/telemetry/config/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Fraunhofer Institute for Software and Systems Engineering
+ * Copyright 2020-2022 Fraunhofer Institute for Software and Systems Engineering
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * This package contains the telemetry configurations.
  */
